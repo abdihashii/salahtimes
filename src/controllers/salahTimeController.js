522a9249128462ae0f7d8beb9f1c7d065a8dcd97
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2022-08-16 19:18:34 -0700

    init commit

---
+++ src/controllers/salahTimeController.js
@@ -0,0 +1,68 @@
+const prayerTimes = {
+  Fajr: '',
+  Dhuhr: '',
+  Asr: '',
+  Maghrib: '',
+  Isha: '',
+};
+
+/**
+ * Converts the time string from 24 hour to 12 hour time
+ * @param {String} time - The time in string format ex: 13:35
+ */
+const convertFrom24HourTo12Hour = (time) => {
+  const [hour, minute] = time.split(':');
+
+  let amPm = 'am';
+  let hourString = hour;
+  let hourInt = parseInt(hour);
+
+  if (hourInt > 12) {
+    hourInt -= 12;
+    hourString = '0' + hourInt.toString();
+    amPm = 'pm';
+  }
+  const convertedTime = `${hourString}:${minute} ${amPm}`;
+
+  return convertedTime;
+};
+
+/**
+ * Returns a list of converted salah times, converting them from 24 to 12 hour times
+ * @param {Object} salahTimes - Salah times
+ * @returns {String[]}
+ */
+export const convertSalahTimes = (salahTimes) => {
+  let convertedSalahTimesList = [];
+
+  for (const [salah, salahTime] of Object.entries(salahTimes)) {
+    if (salah in prayerTimes) {
+      const convertedSalahTime = convertFrom24HourTo12Hour(salahTime);
+      convertedSalahTimesList = [
+        ...convertedSalahTimesList,
+        convertedSalahTime,
+      ];
+    }
+  }
+
+  return convertedSalahTimesList;
+};
+
+export const getCityNameFromLatLng = async (lat, lng) => {
+  try {
+    const res = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyCxVzF10x8rBy1VakwMG5pXfeEJHqZARX0`
+    );
+    const { results } = await res.json();
+
+    for (const result of results) {
+      if (result.types.includes('locality')) {
+        const { long_name: city } = result.address_components[0];
+
+        return city;
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  // this is the reason why the app renders things twice on mount. This only happens in dev mode
+  // get rid of strictmode if you don't want that.
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
