414749a96bf655be22620f8d0beff32f6ba2adeb
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2022-10-29 14:35:49 -0500

    build: fix build

--- my-prayer-times/gatsby-ssr.js
+++ my-prayer-times/gatsby-ssr.js
@@ -31,6 +31,6 @@ exports.onRenderBody = ({ setHeadComponents }) => {
 };
 
 // Wraps every page in a component
-exports.wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
-};
+// exports.wrapPageElement = ({ element, props }) => {
+//   return <Layout >{element}</Layout>;
+// };
