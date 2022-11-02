913a7cdda69cefb3cd82988806ef26e83d6e2b43
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2022-11-01 22:47:19 -0500

    adding config to google analytics to allow search console

--- gatsby-client/gatsby-config.js
+++ gatsby-client/gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_PROPERTY_ID,
+        head: true,
       },
     },
     'gatsby-plugin-image',
