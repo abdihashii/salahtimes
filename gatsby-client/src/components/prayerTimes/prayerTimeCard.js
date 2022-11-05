596d0f302a2fb42345c13d4ad2c00c188fe4803a
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2022-11-05 17:25:04 -0500

    feat: gradient loading animation

--- gatsby-client/src/components/prayerTimes/prayerTimeCard.js
+++ gatsby-client/src/components/prayerTimes/prayerTimeCard.js
@@ -1,20 +1,31 @@
 import React, { useContext } from 'react';
 import { PrayerTimesContext } from '../../contexts/prayerTimesContext';
+import { gradient } from './loadinganimation.module.css';
 
-export const PrayerTimeCard = ({ salah, time }) => {
+export const PrayerTimeCard = ({ salah, time, isLoading }) => {
   const { closestPrayerTime } = useContext(PrayerTimesContext);
 
   return (
     <>
       <div
-        className={`flex h-151px w-11/12 flex-col items-center justify-center rounded-20px md:w-229px  ${
-          closestPrayerTime.closestPrayer === salah
-            ? 'bg-green-dark text-white shadow-darkGreen'
-            : 'bg-white shadow-gray'
+        className={`flex h-151px w-11/12 flex-col items-center justify-center rounded-20px md:w-229px ${
+          isLoading
+            ? `${gradient}`
+            : `${
+                closestPrayerTime.closestPrayer === salah
+                  ? 'bg-green-dark text-white shadow-darkGreen'
+                  : 'bg-white shadow-gray'
+              }`
         }`}
       >
-        <p className="text-24px font-semibold uppercase">{salah}</p>
-        <p className="text-base font-normal">{time}</p>
+        {isLoading ? (
+          <></>
+        ) : (
+          <>
+            <p className="text-24px font-semibold uppercase">{salah}</p>
+            <p className="text-base font-normal">{time}</p>
+          </>
+        )}
       </div>
     </>
   );
--- gatsby-client/src/components/prayerTimes/prayerTimes.js
+++ gatsby-client/src/components/prayerTimes/prayerTimes.js
@@ -8,33 +8,33 @@ export const PrayerTimes = () => {
 
   return (
     <>
-      {isLoading ? (
-        <p className="mt-56px mb-38px flex items-start justify-center">
-          Loading...
-        </p>
-      ) : (
-        <>
-          <div className="mb-51px flex flex-col items-center gap-1.125 lg:flex-row">
-            {Object.entries(prayerTimes).map(([salah, time]) => {
-              return <PrayerTimeCard key={salah} {...{ salah, time }} />;
-            })}
-          </div>
+      <>
+        <div className="mb-51px flex flex-col items-center gap-1.125 lg:flex-row">
+          {Object.entries(prayerTimes).map(([salah, time]) => {
+            return (
+              <PrayerTimeCard
+                key={salah}
+                {...{ salah, time }}
+                isLoading={isLoading}
+              />
+            );
+          })}
+        </div>
 
-          {debug && (
-            <>
-              <div>
-                <pre>{JSON.stringify(prayerTimes, null, 2)}</pre>
-              </div>
-              <div>
-                <pre>{JSON.stringify(input, null, 2)}</pre>
-              </div>
-              <div>
-                <pre>{JSON.stringify(closestPrayerTime, null, 2)}</pre>
-              </div>
-            </>
-          )}
-        </>
-      )}
+        {debug && (
+          <>
+            <div>
+              <pre>{JSON.stringify(prayerTimes, null, 2)}</pre>
+            </div>
+            <div>
+              <pre>{JSON.stringify(input, null, 2)}</pre>
+            </div>
+            <div>
+              <pre>{JSON.stringify(closestPrayerTime, null, 2)}</pre>
+            </div>
+          </>
+        )}
+      </>
     </>
   );
 };
