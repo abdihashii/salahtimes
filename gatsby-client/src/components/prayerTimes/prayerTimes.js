103aa53df55d3ed313faabc2418bdf69f80b5611
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2022-11-05 17:27:21 -0500

    cleanup

--- gatsby-client/src/components/prayerTimes/prayerTimes.js
+++ gatsby-client/src/components/prayerTimes/prayerTimes.js
@@ -8,33 +8,31 @@ export const PrayerTimes = () => {
 
   return (
     <>
-      <>
-        <div className="mb-51px flex flex-col items-center gap-1.125 lg:flex-row">
-          {Object.entries(prayerTimes).map(([salah, time]) => {
-            return (
-              <PrayerTimeCard
-                key={salah}
-                {...{ salah, time }}
-                isLoading={isLoading}
-              />
-            );
-          })}
-        </div>
+      <div className="mb-51px flex flex-col items-center gap-1.125 lg:flex-row">
+        {Object.entries(prayerTimes).map(([salah, time]) => {
+          return (
+            <PrayerTimeCard
+              key={salah}
+              {...{ salah, time }}
+              isLoading={isLoading}
+            />
+          );
+        })}
+      </div>
 
-        {debug && (
-          <>
-            <div>
-              <pre>{JSON.stringify(prayerTimes, null, 2)}</pre>
-            </div>
-            <div>
-              <pre>{JSON.stringify(input, null, 2)}</pre>
-            </div>
-            <div>
-              <pre>{JSON.stringify(closestPrayerTime, null, 2)}</pre>
-            </div>
-          </>
-        )}
-      </>
+      {debug && (
+        <>
+          <div>
+            <pre>{JSON.stringify(prayerTimes, null, 2)}</pre>
+          </div>
+          <div>
+            <pre>{JSON.stringify(input, null, 2)}</pre>
+          </div>
+          <div>
+            <pre>{JSON.stringify(closestPrayerTime, null, 2)}</pre>
+          </div>
+        </>
+      )}
     </>
   );
 };
