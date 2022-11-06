9228bd42441320565c75846df735863800e0fac2
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2022-11-05 20:42:40 -0500

    init

--- gatsby-client/src/pages/index.js
+++ gatsby-client/src/pages/index.js
@@ -4,12 +4,14 @@ import { LocationSelector } from '../components/locationSelector';
 import Seo from '../components/seo';
 import { PrayerTimes } from '../components/prayerTimes/prayerTimes';
 import { MorePrayerTimes } from '../components/morePrayerTimes/morePrayerTimes';
+import CurrentTime from '../components/currentTime/currentTime';
 import '../styles/styles.scss';
 
 const Home = () => {
   return (
     <Layout>
       <LocationSelector />
+      <CurrentTime />
       <PrayerTimes />
       <MorePrayerTimes />
     </Layout>
