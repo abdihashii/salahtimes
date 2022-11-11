74bfc0d4d1acd01624e478a3085036b08f397ee6
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2022-11-11 16:42:54 -0600

    adding current time design and functionality

--- gatsby-client/src/components/currentTime/currentTime.js
+++ gatsby-client/src/components/currentTime/currentTime.js
@@ -5,12 +5,14 @@ const CurrentTime = () => {
   const { input, currentTime } = useContext(PrayerTimesContext);
 
   return (
-    <div className="flex justify-center items-center my-0 mx-0 mb-56px">
-      <p>
-        Current time in{' '}
-        <span className="text-green-dark">{input.selectedCity}</span> :{' '}
-        {currentTime}
-      </p>
+    <div className="my-0 mx-auto mb-56px flex h-2.5em w-fit min-w-11/12 items-center justify-center rounded-20px bg-green-dark lg:min-w-3/12 lg:py-11px lg:px-60px">
+      {currentTime && (
+        <p className="text-white">
+          Current time in{' '}
+          <span className="font-bold">{input.selectedCity}</span> :{' '}
+          {currentTime}
+        </p>
+      )}
     </div>
   );
 };
