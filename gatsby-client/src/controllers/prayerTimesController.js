42eb164d6d983185e042c46845b40974edabbd31
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2022-11-12 14:48:46 -0600

    resolved merge conflict issues

--- gatsby-client/src/controllers/prayerTimesController.js
+++ gatsby-client/src/controllers/prayerTimesController.js
@@ -80,7 +80,11 @@ export const getCityNameFromLatLng = async (lat, lng) => {
 };
 
 export const getCurrentLocalTime = (timeZoneId) => {
+<<<<<<< HEAD
   return moment().tz(timeZoneId).format('hh:mm a');
+=======
+  return moment().tz(timeZoneId).format('h:mm a');
+>>>>>>> add most of currentTime feat
 };
 
 export const getTimeZoneId = async (lat, lng) => {
