ea90b6fdd3b770335a7db42ec55dd5d2f7b163f0
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-02 20:39:35 -0600

    simplify current time in to time in

--- gatsby-client/src/components/currentTime/currentTime.js
+++ gatsby-client/src/components/currentTime/currentTime.js
@@ -8,8 +8,7 @@ const CurrentTime = () => {
     <div className="my-0 mx-auto mb-6 flex h-2.5em w-fit min-w-11/12 items-center justify-center rounded-20px bg-green-dark lg:mb-56px lg:min-w-3/12 lg:py-11px lg:px-60px">
       {currentTime && (
         <p className="text-white">
-          Current time in{' '}
-          <span className="font-bold">{input.selectedCity}</span> :{' '}
+          Time in <span className="font-bold">{input.selectedCity}</span> :{' '}
           {currentTime}
         </p>
       )}
