1ae11be276c1895ca36125e2213402811d7a2a91
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-02 20:39:11 -0600

    mobile and desktop logo cleanup

--- gatsby-client/src/components/navBar/navBar.js
+++ gatsby-client/src/components/navBar/navBar.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { GatsbyImage, StaticImage } from 'gatsby-plugin-image';
+import { GatsbyImage } from 'gatsby-plugin-image';
 import { useStaticQuery, graphql } from 'gatsby';
 
 const NavBar = () => {
-  const data = useStaticQuery(graphql`
+  const { mobileLogo, desktopLogo } = useStaticQuery(graphql`
     query HeaderQuery {
-      file(relativePath: { eq: "logo-single-cropped.png" }) {
+      mobileLogo: file(relativePath: { eq: "logo-single-cropped.png" }) {
+        childImageSharp {
+          gatsbyImageData(placeholder: TRACED_SVG)
+        }
+      }
+      desktopLogo: file(relativePath: { eq: "logo.png" }) {
         childImageSharp {
           gatsbyImageData(placeholder: TRACED_SVG)
         }
@@ -16,14 +21,13 @@ const NavBar = () => {
   return (
     <div className="mx-auto mt-6 mb-12 flex w-11/12 flex-row items-center justify-between lg:my-0 lg:w-full lg:justify-center">
       <GatsbyImage
-        image={data.file.childImageSharp.gatsbyImageData}
+        image={mobileLogo.childImageSharp.gatsbyImageData}
         alt="My Prayer Times Logo"
         className="w-1/6 lg:mx-auto lg:hidden lg:w-1/5"
       />
-      <StaticImage
-        src="../images/logo.png"
+      <GatsbyImage
+        image={desktopLogo.childImageSharp.gatsbyImageData}
         alt="My Prayer Times Logo"
-        placeholder="tracedSVG"
         className="hidden lg:mx-auto lg:!flex lg:w-2/5 lg:justify-center"
       />
       <i className="fa-solid fa-bars fa-2xl pointer-events-auto cursor-pointer text-green-dark lg:hidden"></i>
