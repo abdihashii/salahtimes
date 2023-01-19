e405662835241a839a7f22def2a6f791da2462d1
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-19 15:22:39 -0600

    renaming layout and navabr to index and move directories

--- gatsby-client/src/components/layout/NavBar/index.js
+++ gatsby-client/src/components/layout/NavBar/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import { RxHamburgerMenu } from 'react-icons/rx';
 
-const index = () => {
+const NavBar = () => {
   return (
     <header className="flex flex-row items-center pt-11px pb-10px border-b border-header_border_color">
       <StaticImage
-        src="../../images/favicon-cropped.png"
+        src="../../../images/favicon-cropped.png"
         alt="My Prayer Times Logo"
         placeholder="BLURRED"
         className="w-2/12 ml-5"
       />
-      <p className="uppercase ml-10px text-green-logo_text font-bold">
+      <p className="uppercase ml-10px text-green-secondary font-bold">
         My Prayer Times
       </p>
-
       <RxHamburgerMenu className="ml-auto mr-6" />
     </header>
   );
 };
 
-export default index;
+export default NavBar;
--- gatsby-client/src/components/layout/index.js
+++ gatsby-client/src/components/layout/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import PrayerTimesContextProvider from '../contexts/prayerTimesContext';
+import PrayerTimesContextProvider from '../../contexts/prayerTimesContext';
 import NavBar from './NavBar';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
     <PrayerTimesContextProvider>
-      <div className="mx-auto w-screen pb-28 lg:w-981px lg:p-0">
+      <div className="mx-auto w-screen lg:w-981px lg:p-0">
         <NavBar />
         {children}
+        <Footer />
       </div>
     </PrayerTimesContextProvider>
   );
