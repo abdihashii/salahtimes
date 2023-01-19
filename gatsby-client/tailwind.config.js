9a355328e210a145a6f8f8444bb1257071350cec
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-19 15:20:56 -0600

    colors, border, margins, gap, fontSize, etc styles

--- gatsby-client/tailwind.config.js
+++ gatsby-client/tailwind.config.js
@@ -10,14 +10,24 @@ module.exports = {
         green: {
           DEFAULT: '#0A963A',
           dark: '#006307',
-          logo_text: '#0A8337',
+          secondary: '#0A8337',
         },
         header_border_color: 'rgba(126, 126, 126, 0.3)',
+        text: {
+          grey: '#646464',
+          layout_text: '#0F0700',
+        },
+        bg: {
+          grey: '#F8F8F8',
+        },
       },
       borderRadius: {
         '4xl': '32px',
         '20px': '20px',
         '40px': '40px',
+      },
+      borderWidth: {
+        '3px': '3px',
       },
       boxShadow: {
         green: '0px 8px 28px -6px rgba(10, 150, 58, 0.42)',
@@ -28,10 +38,15 @@ module.exports = {
       margin: {
         '10px': '10px',
         '11px': '11px',
+        '13px': '13px',
         '24px': '24px',
+        '26px': '26px',
         '38px': '38px !important',
+        '50px': '50px',
         '51px': '51px',
+        '55px': '55px',
         '56px': '56px',
+        '63px': '63px',
         '86px': '86px',
         auto: 'auto !important',
       },
@@ -53,9 +68,12 @@ module.exports = {
         '2.5em': '2.5em',
       },
       gap: {
+        '11px': '11px',
         1.125: '1.125rem',
       },
       fontSize: {
+        '13px': '13px',
+        '17px': '17.56px',
         '24px': '24px',
         '1.5em': '1.5em',
       },
@@ -64,6 +82,8 @@ module.exports = {
         '11px': '11px',
         '12px': '12px',
         '17px': '17px',
+        '22px': '22px',
+        '23px': '23px',
         '60px': '60px',
       },
       minWidth: {
@@ -72,6 +92,14 @@ module.exports = {
       },
       minHeight: {
         '110vh': '110vh',
+      },
+      letterSpacing: {
+        '0.011em': '0.011em',
+      },
+      lineHeight: {
+        '26px': '26px',
+        '39px': '39px',
+        '44px': '44px',
       },
     },
   },
