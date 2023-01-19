242c99f2bee4b18a11367d8cbcd8f72462d51555
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-19 15:20:10 -0600

    adding 200 and 500 font weight from google fonts

--- gatsby-client/gatsby-config.js
+++ gatsby-client/gatsby-config.js
@@ -107,7 +107,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Poppins\:400,600,700`],
+        fonts: [`Poppins\:200,400,500,600,700`],
         display: 'swap',
       },
     },
