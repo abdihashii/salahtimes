6cc7e4ca347f2d42e9a788999b4e3875d222edd2
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-19 18:53:47 -0600

    adding more tailwind extensions for about us section

--- gatsby-client/tailwind.config.js
+++ gatsby-client/tailwind.config.js
@@ -14,8 +14,11 @@ module.exports = {
         },
         header_border_color: 'rgba(126, 126, 126, 0.3)',
         text: {
+          light_grey: '#D2D2D2',
+          medium_grey: '#848280',
           grey: '#646464',
           layout_text: '#0F0700',
+          button: '#FFFAF5',
         },
         bg: {
           grey: '#F8F8F8',
@@ -28,24 +31,30 @@ module.exports = {
       },
       borderWidth: {
         '3px': '3px',
+        '5px': '5px',
       },
       boxShadow: {
         green: '0px 8px 28px -6px rgba(10, 150, 58, 0.42)',
         darkGreen: '0px 8px 28px -6px #006307',
         // green: '0px 8px 28px -6px rgba(10, 150, 58, 0.42), 0px 18px 88px -4px rgba(11, 147, 59, 0.42);'
         gray: '0px 8px 28px -6px rgba(24, 39, 75, 0.12), 0px 18px 88px -4px rgba(24, 39, 75, 0.14)',
+        bg_layer: 'inset 0 0 0 1000px rgba(0, 11, 3, 0.8)',
       },
       margin: {
         '10px': '10px',
         '11px': '11px',
         '13px': '13px',
+        '22px': '22px',
         '24px': '24px',
+        '25px': '25px',
         '26px': '26px',
         '38px': '38px !important',
+        '39px': '39px',
         '50px': '50px',
         '51px': '51px',
         '55px': '55px',
         '56px': '56px',
+        '59px': '59px',
         '63px': '63px',
         '86px': '86px',
         auto: 'auto !important',
@@ -75,6 +84,7 @@ module.exports = {
         '13px': '13px',
         '17px': '17.56px',
         '24px': '24px',
+        '32px': '32px',
         '1.5em': '1.5em',
       },
       padding: {
@@ -82,8 +92,10 @@ module.exports = {
         '11px': '11px',
         '12px': '12px',
         '17px': '17px',
+        '21px': '21px',
         '22px': '22px',
         '23px': '23px',
+        '30px': '30px',
         '60px': '60px',
       },
       minWidth: {
@@ -97,7 +109,9 @@ module.exports = {
         '0.011em': '0.011em',
       },
       lineHeight: {
+        '22px': '22px',
         '26px': '26px',
+        '30px': '30px',
         '39px': '39px',
         '44px': '44px',
       },
