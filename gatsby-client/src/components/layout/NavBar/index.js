d519d410abd9c1db69ca68e3bf33e457086d58da
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-25 16:28:23 -0600

    adding hambruger menu and also some layout fixes

--- gatsby-client/src/components/layout/NavBar/index.js
+++ gatsby-client/src/components/layout/NavBar/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import { RxHamburgerMenu } from 'react-icons/rx';
+import { RxHamburgerMenu, RxCross1 } from 'react-icons/rx';
 import { graphql, useStaticQuery } from 'gatsby';
+import { PrayerTimesContext } from '../../../contexts/prayerTimesContext';
 
 const NavBar = ({ transparentNav }) => {
+  const { isHamburgerMenuOpen, setIsHamburgerMenuOpen } =
+    useContext(PrayerTimesContext);
+
   const { faviconCropped } = useStaticQuery(graphql`
     query {
       faviconCropped: file(relativePath: { in: "favicon-cropped-600px.png" }) {
@@ -14,32 +18,82 @@ const NavBar = ({ transparentNav }) => {
     }
   `);
 
+  const HamburgerMenu = () => {
+    return (
+      <nav className="absolute top-0 left-0 z-10 mx-auto w-full bg-green-extra_dark pt-36 pb-52px font-semibold text-white">
+        <ul className="mb-83px flex flex-col gap-16 text-2xl">
+          <li className="mx-auto w-fit">
+            <a href="/">Prayer Times</a>
+          </li>
+          <li className="mx-auto w-fit">
+            <a href="/about-us">About Us</a>
+          </li>
+          <li className="mx-auto w-fit">
+            <a href="/resources">Resources</a>
+          </li>
+          <li className="mx-auto w-fit">
+            <a href="/faqs">FAQs</a>
+          </li>
+          <li className="mx-auto w-fit">
+            <a href="/contact-us">Contact Us</a>
+          </li>
+        </ul>
+
+        <hr className="mx-auto mb-45px w-7/12" />
+
+        <a
+          href="/blog"
+          className="mx-auto block w-fit rounded-full bg-green-secondary py-4 px-50px"
+        >
+          Our Blog
+        </a>
+      </nav>
+    );
+  };
+
   return (
-    <header
-      className={`flex flex-row items-center pt-11px pb-10px ${
-        transparentNav
-          ? 'absolute top-0 left-0 z-50'
-          : 'static border-b border-header_border_color'
-      }`}
-    >
-      <a className="contents" href="/">
-        <GatsbyImage
-          image={faviconCropped.childImageSharp.gatsbyImageData}
-          alt="My Prayer Times Logo"
-          className="ml-5 w-2/12"
-        />
-      </a>
-      <p
-        className={`ml-10px uppercase ${
-          transparentNav ? 'text-white' : 'text-green-secondary'
-        } font-bold`}
+    <>
+      <header
+        className={`pt-11px pb-10px ${
+          transparentNav || isHamburgerMenuOpen
+            ? 'absolute top-0 left-0 z-30 w-full'
+            : 'static border-b border-header_border_color'
+        }`}
       >
-        My Prayer Times
-      </p>
-      <RxHamburgerMenu
-        className={`ml-auto mr-6 ${transparentNav ? 'text-white' : ''}`}
-      />
-    </header>
+        <div className="mx-auto flex w-10/12 flex-row items-center">
+          <a className="flex flex-row items-center gap-10px" href="/">
+            <GatsbyImage
+              image={faviconCropped.childImageSharp.gatsbyImageData}
+              alt="My Prayer Times Logo"
+              className="w-12"
+            />
+            <p
+              className={`uppercase ${
+                transparentNav ? 'text-white' : 'text-green-secondary'
+              } font-bold`}
+            >
+              My Prayer Times
+            </p>
+          </a>
+          {isHamburgerMenuOpen ? (
+            <RxCross1
+              className={`ml-auto text-2xl ${
+                transparentNav || isHamburgerMenuOpen ? 'text-white' : ''
+              }`}
+              onClick={() => setIsHamburgerMenuOpen(!isHamburgerMenuOpen)}
+            />
+          ) : (
+            <RxHamburgerMenu
+              className={`ml-auto text-2xl ${
+                transparentNav ? 'text-white' : ''
+              }`}
+              onClick={() => setIsHamburgerMenuOpen(!isHamburgerMenuOpen)}
+            />
+          )}
+        </div>
+      </header>
+      {isHamburgerMenuOpen && <HamburgerMenu />}
+    </>
   );
 };
 
