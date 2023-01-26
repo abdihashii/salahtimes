674dd0f07acf23a583a37f3e877fa5594c57591a
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-26 14:51:36 -0600

    Adding lcoation selector and title to home page prayer times

--- gatsby-client/src/pages/index.js
+++ gatsby-client/src/pages/index.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../components/layout';
-// import { LocationSelector } from '../components/locationSelector';
+import { LocationSelector } from '../components/locationSelector';
 import Seo from '../components/seo';
-// import { PrayerTimes } from '../components/prayerTimes/prayerTimes';
-// import { MorePrayerTimes } from '../components/morePrayerTimes/morePrayerTimes';
-// import CurrentTime from '../components/currentTime/currentTime';
+import { PrayerTimes } from '../components/prayerTimes/prayerTimes';
+import { MorePrayerTimes } from '../components/morePrayerTimes/morePrayerTimes';
+import CurrentTime from '../components/currentTime/currentTime';
 import '../styles/styles.scss';
+import masjidBackground from '../images/masjid_bg.webp';
+import { RxPencil2 } from 'react-icons/rx';
+import PrayerTimesTitle from '../components/prayerTimesTitle';
 
 const Home = () => {
   return (
-    <Layout transparentNav={false}>
-      {/* <LocationSelector />
-      <CurrentTime />
+    <Layout transparentNav={true}>
+      {/* Prayer Times */}
+      <section
+        className="mb-59px flex min-h-screen flex-col bg-cover bg-no-repeat text-center text-white shadow-bg_layer"
+        style={{
+          backgroundImage: `url(${masjidBackground})`,
+        }}
+      >
+        <PrayerTimesTitle />
+        <CurrentTime />
+        <p className="mx-auto mb-5 w-10/12 text-lg font-medium text-text-light_grey">
+          Calculation Method: Islamic Society of North America, Juristic
+          settings: <strong>Shafii</strong>
+        </p>
+        <button className="mx-auto mb-45px flex w-fit items-center gap-2 text-text-light_blue">
+          <RxPencil2 /> Change Settings
+        </button>
+        <LocationSelector />
+      </section>
       <PrayerTimes />
-      <MorePrayerTimes /> */}
+      <MorePrayerTimes />
     </Layout>
   );
 };
