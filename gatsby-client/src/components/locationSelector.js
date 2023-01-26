9a9a0fd48e38c37e13675dd9bca8dd21023f3e12
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-26 14:50:41 -0600

    updating the locationSelector to match design

--- gatsby-client/src/components/locationSelector.js
+++ gatsby-client/src/components/locationSelector.js
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 import PlacesAutocomplete from 'react-places-autocomplete';
 import { PrayerTimesContext } from '../contexts/prayerTimesContext';
-import { BiCurrentLocation } from 'react-icons/bi';
-import { MdLocationOn } from 'react-icons/md';
+import { RxMagnifyingGlass } from 'react-icons/rx';
+import { RiMapPinLine } from 'react-icons/ri';
 
 export const LocationSelector = () => {
   const {
@@ -20,7 +20,7 @@ export const LocationSelector = () => {
   useEffect(() => {
     getLocationByIpAddress(
       `https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.GATSBY_MAPS_API_KEY}`,
-      { method: 'POST' }
+      { method: 'POST' },
     );
 
     const queryString = window.location.search;
@@ -45,15 +45,12 @@ export const LocationSelector = () => {
 
   return (
     <>
-      <div className="fixed inset-x-0 bottom-0 mx-auto mb-4 w-full lg:static lg:mb-56px">
+      <div className="mx-auto mb-45px w-full">
         <div className="relative mx-auto w-11/12 lg:w-full">
           {/* Map icon */}
           <span className="absolute left-0 top-0 z-10 inline-flex h-2.5em w-2.5em items-center justify-center text-2xl">
-            <MdLocationOn className="text-green-dark" />
+            <RxMagnifyingGlass className="text-text-grey" />
           </span>
-          {/* <span className="fa-xl absolute left-0 top-0 z-10 inline-flex h-2.5em w-2.5em items-center justify-center">
-            <i className="fa-solid fa-location-dot text-green-dark"></i>
-          </span> */}
 
           <PlacesAutocomplete
             value={input.city}
@@ -67,21 +64,22 @@ export const LocationSelector = () => {
               loading,
             }) => (
               <div>
+                {/* The Main Input */}
                 <input
                   {...getInputProps({
-                    placeholder: 'Enter your location',
+                    placeholder: 'Type your location',
                     autoFocus: true,
-                    className: `${
+                    className: `h-2.5em w-full border border-border-input_white py-11px px-60px text-2xl hover:border-green-secondary focus:border-green-secondary outline-none text-black ${
                       suggestions.length > 0
-                        ? 'rounded-bl-none rounded-br-none'
-                        : ''
-                    } h-2.5em w-full rounded-20px border-2 border-green-dark py-11px px-60px text-2xl hover:border-green focus:border-green outline-none`,
+                        ? 'rounded-bl-none rounded-br-none rounded-tr-30px rounded-tl-30px'
+                        : 'rounded-full'
+                    }`,
                   })}
                 />
                 <div
                   className={`${
                     suggestions.length > 0
-                      ? 'rounded-bl-20px rounded-br-20px border-2 border-t-0 border-green'
+                      ? 'rounded-bl-full rounded-br-full border border-t-0 border-green-secondary'
                       : ''
                   }`}
                 >
@@ -106,7 +104,9 @@ export const LocationSelector = () => {
                               style,
                             })}
                           >
-                            <span>{suggestion.description}</span>
+                            <span className="text-black">
+                              {suggestion.description}
+                            </span>
                           </div>
                         );
                       })}
@@ -117,26 +117,18 @@ export const LocationSelector = () => {
             )}
           </PlacesAutocomplete>
 
-          <span
-            className="absolute right-0 top-0 z-10 inline-flex h-2.5em w-2.5em items-center justify-center text-2xl"
-            onClick={onMapIconClick}
-            onKeyDown={onMapIconClick}
-            role="button"
-            tabIndex={0}
-            aria-label="Get current location's prayer times"
-          >
-            <BiCurrentLocation className="pointer-events-auto cursor-pointer text-green-dark hover:text-green" />
-          </span>
-          {/* <span
-            className="fa-xl absolute right-0 top-0 z-10 inline-flex h-2.5em w-2.5em items-center justify-center"
-            onClick={onMapIconClick}
-            onKeyDown={onMapIconClick}
-            role="button"
-            tabIndex={0}
-            aria-label="Get current location's prayer times"
+          <span className="absolute right-0 top-0 z-10 flex h-60px w-60px items-center justify-center">
+            <span
+              className=" pointer-events-auto inline-flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-green-secondary text-xl hover:bg-green-dark"
+              onClick={onMapIconClick}
+              onKeyDown={onMapIconClick}
+              role="button"
+              tabIndex={0}
+              aria-label="Get current location's prayer times"
             >
-            <i className="fa-solid fa-location-crosshairs pointer-events-auto cursor-pointer text-green-dark hover:text-green"></i>
-          </span> */}
+              <RiMapPinLine className="text-green-white" />
+            </span>
+          </span>
         </div>
       </div>
     </>
