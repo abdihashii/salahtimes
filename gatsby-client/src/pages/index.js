4195b68d2fd3f6b09c97407a6c6b930c64759253
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-01-27 15:35:58 -0600

    Nearly finsihed with Home Page!!

--- gatsby-client/src/pages/index.js
+++ gatsby-client/src/pages/index.js
@@ -7,13 +7,17 @@ import { MorePrayerTimes } from '../components/morePrayerTimes/morePrayerTimes';
 import CurrentTime from '../components/currentTime/currentTime';
 import '../styles/styles.scss';
 import masjidBackground from '../images/masjid_bg.webp';
-import { RxPencil2 } from 'react-icons/rx';
+import { RxPencil2, RxArrowRight } from 'react-icons/rx';
 import PrayerTimesTitle from '../components/prayerTimesTitle';
+import TextSection from '../components/textSection';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import { graphql } from 'gatsby';
+import TrendingStories from '../components/trendingStories';
 
-const Home = () => {
+const Home = ({ data: { masjid_1, masjid_2 } }) => {
   return (
     <Layout transparentNav={true}>
-      {/* Prayer Times */}
+      {/* Prayer Times Section */}
       <section
         className="mb-59px flex min-h-screen flex-col bg-cover bg-no-repeat text-center text-white shadow-bg_layer"
         style={{
@@ -37,9 +41,55 @@ const Home = () => {
         <PrayerTimes />
         <MorePrayerTimes />
       </section>
+
+      {/* About MPT Section */}
+      <section className="mb-50px">
+        <TextSection
+          heading={'About MyPrayerTimes'}
+          textContent={`Varius tristique aliquet vulputate senectus duis urna et tellus. Neque eu ac consequat convallis dui facilisi. Pellentesque leo odio adipiscing tortor. Lorem faucibus lectus sed tristique. Hendrerit convallis sed praesent sed quam lectus laoreet.
+Viverra purus sit auctor convallis. Nullam ac ut elit orci donec sed velit sit nunc. Turpis urna tristique adipiscing cursus malesuada. Ornare augue sollicitudin congue pretium sed purus hendrerit amet magna. Pulvinar lacinia vivamus auctor mauris.`}
+        />
+        <a
+          href="/about-us"
+          className="mx-auto flex w-fit flex-row items-center gap-4 rounded-full border px-25px py-14px text-xs font-semibold"
+        >
+          <p className="text-green-secondary">Learn more</p>
+          <RxArrowRight />
+        </a>
+      </section>
+
+      {/* Two Image Section */}
+      <section className="mx-auto mb-28 flex w-10/12 flex-row gap-4">
+        <GatsbyImage
+          image={masjid_1.childImageSharp.gatsbyImageData}
+          className="mb-11"
+        />
+        <GatsbyImage
+          image={masjid_2.childImageSharp.gatsbyImageData}
+          className="mt-11"
+        />
+      </section>
+
+      {/* Trending Stories Section */}
+      <TrendingStories />
     </Layout>
   );
 };
+
+export const data = graphql`
+  query HomeImages {
+    masjid_1: file(name: { eq: "masjid_1" }) {
+      childImageSharp {
+        gatsbyImageData(formats: WEBP, placeholder: BLURRED)
+      }
+    }
+    masjid_2: file(name: { eq: "masjid_2" }) {
+      childImageSharp {
+        gatsbyImageData(formats: WEBP, placeholder: BLURRED)
+      }
+    }
+  }
+`;
 
 export const Head = () => {
   return (
