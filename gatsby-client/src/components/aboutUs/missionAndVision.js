01ac3e7885a996f0c516e0f5766d1f666f718daa
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-02-05 16:37:56 -0600

    clean up mission and vision

--- gatsby-client/src/components/aboutUs/missionAndVision.js
+++ gatsby-client/src/components/aboutUs/missionAndVision.js
@@ -3,43 +3,69 @@ import React from 'react';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import TextSection from '../textSection';
 
-const MissionAndVision = ({ aboutUsContent }) => {
+const aboutUsContent = {
+  mission: {
+    title: 'Our Mission',
+    content: `To empower Muslims of diverse backgrounds with accurate and reliable
+    Islamic prayer times while providing informative content to deepen
+    understanding and connection with Allah and our faith. Guided by
+    principles of integrity, transparency, and love for Allah, we strive
+    to establish a community of believers who stay on track with their
+    daily salah and continue to learn and grow in their spiritual
+    journey.`,
+  },
+  vision: {
+    title: 'Our Vision',
+    content: `To be a global platform that engages the Ummah and inspires a deeper
+    connection and love for Salah through accurate prayer times and
+    high-quality, well-researched content. We aspire to be known as the
+    go-to website for all things related to Islamic prayer and to have a
+    substantial impact on our users.`,
+  },
+};
+
+const MissionAndVision = () => {
   const { zayed } = useStaticQuery(graphql`
     query {
       zayed: file(
         relativePath: { in: "about-us-page/sheikh-zayed-grand-mosque.jpg" }
       ) {
         childImageSharp {
-          gatsbyImageData(placeholder: BLURRED, formats: WEBP)
+          gatsbyImageData(
+            placeholder: BLURRED
+            formats: WEBP
+            aspectRatio: 1.260206565
+          )
         }
       }
     }
   `);
 
   return (
-    <section className="lg:mx-auto lg:mb-183px lg:flex lg:w-9/12 lg:flex-row lg:gap-20">
+    <section className="lg:mx-auto lg:mb-183px lg:flex lg:w-9/12 lg:flex-row lg:gap-6">
       {/* Image w/ Shadow Section */}
-      <section className="relative mx-auto mb-10 w-11/12 lg:mb-0">
-        <div className="absolute bottom-0 right-0 h-full w-10/12 bg-green-secondary"></div>
+      <section className="relative mx-auto mb-10 w-11/12 pb-21px lg:mb-0 lg:w-1/2 lg:p-0">
+        <div
+          className="absolute bottom-0 right-0 h-5/6 w-10/12 bg-green-secondary"
+          // style={{ height: 'calc(100% - 16px)' }}
+        ></div>
         <GatsbyImage
           image={zayed.childImageSharp.gatsbyImageData}
-          className="w-11/12"
+          className="h-5/6 w-10/12"
         />
       </section>
 
-      <div>
+      <div className="lg:w-1/2">
         {/* Our Mission */}
         <TextSection
           heading={aboutUsContent.mission.title}
           textContent={aboutUsContent.mission.content}
-          desktop
         />
 
         {/* Our Vision */}
         <TextSection
           heading={aboutUsContent.vision.title}
           textContent={aboutUsContent.vision.content}
-          desktop
           className="lg:mb-0"
         />
       </div>
--- gatsby-client/src/pages/about-us.js
+++ gatsby-client/src/pages/about-us.js
@@ -7,27 +7,6 @@ import TrendingStories from '../components/trendingStories';
 import AboutUsHeader from '../components/aboutUs/aboutUsHeader';
 import MissionAndVision from '../components/aboutUs/missionAndVision';
 
-const aboutUsContent = {
-  mission: {
-    title: 'Our Mission',
-    content: `To empower Muslims of diverse backgrounds with accurate and reliable
-    Islamic prayer times while providing informative content to deepen
-    understanding and connection with Allah and our faith. Guided by
-    principles of integrity, transparency, and love for Allah, we strive
-    to establish a community of believers who stay on track with their
-    daily salah and continue to learn and grow in their spiritual
-    journey.`,
-  },
-  vision: {
-    title: 'Our Vision',
-    content: `To be a global platform that engages the Ummah and inspires a deeper
-    connection and love for Salah through accurate prayer times and
-    high-quality, well-researched content. We aspire to be known as the
-    go-to website for all things related to Islamic prayer and to have a
-    substantial impact on our users.`,
-  },
-};
-
 const AboutUs = () => {
   return (
     <Layout transparentNav={true}>
@@ -36,7 +15,7 @@ const AboutUs = () => {
         <AboutUsHeader />
 
         {/* Mission and Vision */}
-        <MissionAndVision {...{ aboutUsContent }} />
+        <MissionAndVision />
 
         {/* Our Core Values */}
         <CoreValues />
