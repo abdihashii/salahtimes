68e4b8ec43612ea0d26d97a18c6e8bf4b3ffbfb4
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-02-05 12:22:28 -0600

    some margin fixes

--- gatsby-client/src/components/prayerTimesTitle/index.js
+++ gatsby-client/src/components/prayerTimesTitle/index.js
@@ -5,7 +5,7 @@ const PrayerTimesTitle = () => {
   const { input } = useContext(PrayerTimesContext);
 
   return (
-    <h1 className="mx-auto mt-28 w-10/12 text-4xl font-medium leading-45px">
+    <h1 className="mx-auto mb-4 mt-28 w-10/12 text-4xl font-medium leading-45px">
       Prayer times in "<span className="text-green">{input.selectedCity}</span>"
     </h1>
   );
