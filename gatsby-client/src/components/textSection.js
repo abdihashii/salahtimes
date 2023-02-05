12707edbad73ef3f46c849261c17194e50afcb17
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-02-05 15:56:29 -0600

    finish sizing trending stories section

--- gatsby-client/src/components/textSection.js
+++ gatsby-client/src/components/textSection.js
@@ -17,23 +17,28 @@ const TextSection = ({
   desktop,
   className,
 }) => {
-  const isCoreValues = heading === 'Our Core Values';
+  const isCoreValuesOrTrending =
+    heading === 'Our Core Values' || 'Must-Read Stories Of The Month';
 
   return (
     <section
       className={`mx-auto mb-10 flex w-11/12 flex-col text-center ${
         desktop ? 'lg:mb-45px lg:text-left' : ''
-      } ${isCoreValues ? 'lg:mb-28 lg:w-9/12' : ''} ${className}`}
+      } ${isCoreValuesOrTrending ? 'lg:mb-28 lg:w-9/12' : ''} ${className}`}
     >
       <h2
         className={`mb-3 text-2xl font-bold leading-30px lg:font-semibold ${
           desktop ? 'lg:text-45px lg:leading-57px' : ''
-        } ${isCoreValues ? 'lg:mb-30px lg:text-40px lg:font-bold' : ''}`}
+        } ${
+          isCoreValuesOrTrending ? 'lg:mb-30px lg:text-40px lg:font-bold' : ''
+        }`}
       >
         {heading}
       </h2>
 
-      <SectionBorder className={`${isCoreValues ? 'lg:hidden' : ''}`} />
+      <SectionBorder
+        className={`${isCoreValuesOrTrending ? 'lg:hidden' : ''}`}
+      />
 
       <p className="text-text-medium_grey lg:leading-29px">{textContent}</p>
 
