3efc4c69ee3ede1791f441bd4a5fcdd255cae549
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-02-05 14:55:28 -0600

    decouple mission and vision

---
+++ gatsby-client/src/components/aboutUs/missionAndVision.js
@@ -0,0 +1,50 @@
+import { graphql, useStaticQuery } from 'gatsby';
+import React from 'react';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import TextSection from '../textSection';
+
+const MissionAndVision = ({ aboutUsContent }) => {
+  const { zayed } = useStaticQuery(graphql`
+    query {
+      zayed: file(
+        relativePath: { in: "about-us-page/sheikh-zayed-grand-mosque.jpg" }
+      ) {
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED, formats: WEBP)
+        }
+      }
+    }
+  `);
+
+  return (
+    <section className="lg:mx-auto lg:mb-183px lg:flex lg:w-10/12 lg:flex-row lg:gap-20">
+      {/* Image w/ Shadow Section */}
+      <section className="relative mx-auto mb-10 w-11/12 lg:mb-0">
+        <div className="absolute bottom-0 right-0 h-full w-10/12 bg-green-secondary"></div>
+        <GatsbyImage
+          image={zayed.childImageSharp.gatsbyImageData}
+          className="w-11/12"
+        />
+      </section>
+
+      <div>
+        {/* Our Mission */}
+        <TextSection
+          heading={aboutUsContent.mission.title}
+          textContent={aboutUsContent.mission.content}
+          desktop
+        />
+
+        {/* Our Vision */}
+        <TextSection
+          heading={aboutUsContent.vision.title}
+          textContent={aboutUsContent.vision.content}
+          desktop
+          className="lg:mb-0"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default MissionAndVision;
--- gatsby-client/src/components/textSection.js
+++ gatsby-client/src/components/textSection.js
@@ -1,19 +1,42 @@
 import React from 'react';
 
 /** The small green border under each 'About Us' section */
-const SectionBorder = () => {
+const SectionBorder = ({ className }) => {
   return (
-    <span className="mx-auto mb-25px w-20 rounded-xl border-4 border-green-secondary"></span>
+    <hr
+      className={`mx-auto mb-25px w-20 rounded-xl border-4 border-green-secondary lg:!ml-0 lg:w-16 lg:border-5px lg:bg-green-secondary ${className}`}
+    />
   );
 };
 
 /** Component for each 'About Us' section */
-const TextSection = ({ heading, textContent, children }) => {
+const TextSection = ({
+  heading,
+  textContent,
+  children,
+  desktop,
+  className,
+}) => {
+  console.log(heading === 'Our Core Values');
   return (
-    <section className="mx-auto mb-10 flex w-11/12 flex-col text-center">
-      <h2 className="mb-3 text-2xl font-bold leading-30px">{heading}</h2>
-      <SectionBorder />
-      <p className="text-text-medium_grey">{textContent}</p>
+    <section
+      className={`mx-auto mb-10 flex w-11/12 flex-col text-center ${
+        desktop ? 'lg:mb-45px lg:text-left' : ''
+      } ${className}`}
+    >
+      <h2
+        className={`mb-3 text-2xl font-bold leading-30px lg:font-semibold ${
+          desktop ? 'lg:text-45px lg:leading-57px' : ''
+        }`}
+      >
+        {heading}
+      </h2>
+
+      <SectionBorder
+        className={`${heading === 'Our Core Values' ? 'lg:hidden' : ''}`}
+      />
+
+      <p className="text-text-medium_grey lg:leading-29px">{textContent}</p>
 
       {children && children}
     </section>
