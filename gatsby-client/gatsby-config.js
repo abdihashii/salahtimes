32a7ec7d52c3e97a811174fa4a6eb1f386cb8f14
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-02-06 00:07:22 -0600

    enabling contentful tags

--- gatsby-client/gatsby-config.js
+++ gatsby-client/gatsby-config.js
@@ -138,6 +138,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_DELIVERY_API_ACCESS_TOKEN,
+        enableTags: true,
       },
     },
     {
