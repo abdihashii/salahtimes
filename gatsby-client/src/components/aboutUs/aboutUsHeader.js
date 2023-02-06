8383b7d4b76a34616ec8dee287d57466548d76af
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-02-05 23:14:56 -0600

    Desktop layout (nav and footer) (#30)
    * adding more top padding to about us header
    
    * adding desktop nav to navbar layout
    
    * fix about us header hover button states
    
    * moving some components out of main component
    
    * fix active state stylings for desktop nav
    
    * add alt to mission and vision picture
    
    * add active style to blog nav desktop button
    
    * FIX HORIZONTAL SCROLL!!
    
    * tailwind configs
    
    * finishing the layout of footer
    
    * replacing all local links with Link

--- gatsby-client/src/components/aboutUs/aboutUsHeader.js
+++ gatsby-client/src/components/aboutUs/aboutUsHeader.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import React from 'react';
 import masjidBackground from '../../images/masjid_bg_wide.webp';
 
@@ -9,7 +10,7 @@ const AboutUsHeader = () => {
         backgroundImage: `url(${masjidBackground})`,
       }}
     >
-      <h1 className="mx-auto mb-22px mt-28 text-32px font-semibold leading-39px lg:mb-30px lg:text-6xl">
+      <h1 className="mx-auto mb-22px mt-28 text-32px font-semibold leading-39px lg:mt-52 lg:mb-30px lg:text-6xl">
         Who We Are
       </h1>
       <p className="mx-auto mb-39px w-10/12 text-13px font-normal leading-22px text-text-light_grey lg:mb-60px lg:w-6/12 lg:text-lg lg:font-medium">
@@ -19,12 +20,18 @@ const AboutUsHeader = () => {
         informative content.
       </p>
       <div className="mx-auto mb-56px flex flex-col justify-center gap-5 lg:mb-106px lg:flex-row lg:gap-21px">
-        <button className="w-max rounded-4xl bg-green-secondary py-3 px-8 font-semibold text-text-button lg:py-4">
+        <Link
+          to="/faqs"
+          className="w-max rounded-4xl bg-green-secondary py-3 px-8 font-semibold text-text-button hover:bg-green-dark lg:py-4"
+        >
           See Our FAQs
-        </button>
-        <button className="rounded-4xl border border-text-button py-3 px-8 font-semibold text-text-button lg:py-4">
+        </Link>
+        <Link
+          to="/contact-us"
+          className="rounded-4xl border border-text-button py-3 px-8 font-semibold text-text-button hover:border-gray-400 lg:py-4"
+        >
           Contact Us
-        </button>
+        </Link>
       </div>
     </div>
   );
--- gatsby-client/src/components/layout/NavBar/index.js
+++ gatsby-client/src/components/layout/NavBar/index.js
@@ -1,8 +1,86 @@
 import React, { useContext } from 'react';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import { RxHamburgerMenu, RxCross1 } from 'react-icons/rx';
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import { PrayerTimesContext } from '../../../contexts/prayerTimesContext';
+
+const HamburgerMenu = () => {
+  return (
+    <nav className="absolute top-0 left-0 z-20 mx-auto min-h-screen w-full bg-green-extra_dark pt-36 pb-52px font-semibold text-white lg:hidden">
+      <ul className="mb-83px flex flex-col gap-16 text-2xl">
+        <li className="mx-auto w-fit">
+          <a href="/">Prayer Times</a>
+        </li>
+        <li className="mx-auto w-fit">
+          <a href="/about-us">About Us</a>
+        </li>
+        <li className="mx-auto w-fit">
+          <a href="/resources">Resources</a>
+        </li>
+        <li className="mx-auto w-fit">
+          <a href="/faqs">FAQs</a>
+        </li>
+        <li className="mx-auto w-fit">
+          <a href="/contact-us">Contact Us</a>
+        </li>
+      </ul>
+
+      <hr className="mx-auto mb-45px w-7/12" />
+
+      <a
+        href="/blog"
+        className="mx-auto block w-fit rounded-full bg-green-secondary py-4 px-50px"
+      >
+        Our Blog
+      </a>
+    </nav>
+  );
+};
+
+const DesktopNav = () => {
+  return (
+    <nav className="ml-auto hidden text-base font-medium leading-6 text-white lg:block">
+      <ul className="flex flex-row gap-10">
+        <li>
+          <Link
+            className="opacity-50 hover:opacity-100"
+            activeStyle={{ opacity: 1 }}
+            to="/about-us/"
+          >
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="opacity-50 hover:opacity-100"
+            activeStyle={{ opacity: 1 }}
+            to="/faqs"
+          >
+            FAQs
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="opacity-50 hover:opacity-100"
+            activeStyle={{ opacity: 1 }}
+            to="/resources"
+          >
+            Resources
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="opacity-50 hover:opacity-100"
+            activeStyle={{ opacity: 1 }}
+            to="/contact-us"
+          >
+            Contact Us
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 const NavBar = ({ transparentNav }) => {
   const { isHamburgerMenuOpen, setIsHamburgerMenuOpen } =
@@ -18,50 +96,18 @@ const NavBar = ({ transparentNav }) => {
     }
   `);
 
-  const HamburgerMenu = () => {
-    return (
-      <nav className="absolute top-0 left-0 z-20 mx-auto min-h-screen w-full bg-green-extra_dark pt-36 pb-52px font-semibold text-white">
-        <ul className="mb-83px flex flex-col gap-16 text-2xl">
-          <li className="mx-auto w-fit">
-            <a href="/">Prayer Times</a>
-          </li>
-          <li className="mx-auto w-fit">
-            <a href="/about-us">About Us</a>
-          </li>
-          <li className="mx-auto w-fit">
-            <a href="/resources">Resources</a>
-          </li>
-          <li className="mx-auto w-fit">
-            <a href="/faqs">FAQs</a>
-          </li>
-          <li className="mx-auto w-fit">
-            <a href="/contact-us">Contact Us</a>
-          </li>
-        </ul>
-
-        <hr className="mx-auto mb-45px w-7/12" />
-
-        <a
-          href="/blog"
-          className="mx-auto block w-fit rounded-full bg-green-secondary py-4 px-50px"
-        >
-          Our Blog
-        </a>
-      </nav>
-    );
-  };
-
   return (
     <>
       <header
-        className={`pt-11px pb-10px ${
+        className={`pt-6 pb-10px lg:pt-10 ${
           transparentNav || isHamburgerMenuOpen
             ? 'absolute top-0 left-0 z-30 w-full'
             : 'static border-b border-header_border_color'
         }`}
       >
         <div className="mx-auto flex w-10/12 flex-row items-center">
-          <a className="flex flex-row items-center gap-10px" href="/">
+          {/* Logo */}
+          <Link className="flex flex-row items-center gap-10px" to="/">
             <GatsbyImage
               image={faviconCropped.childImageSharp.gatsbyImageData}
               alt="My Prayer Times Logo"
@@ -74,17 +120,31 @@ const NavBar = ({ transparentNav }) => {
             >
               My Prayer Times
             </p>
-          </a>
+          </Link>
+
+          {/* Navigation - Desktop */}
+          <DesktopNav />
+
+          {/* Our Blog button - Navigation */}
+          <Link
+            className="ml-36 hidden rounded-full bg-green-secondary py-4 px-12 font-semibold text-white hover:bg-green-dark lg:block"
+            activeStyle={{ backgroundColor: '#006307' }}
+            to="/blog"
+          >
+            Our Blog
+          </Link>
+
+          {/* Hamburger icon */}
           {isHamburgerMenuOpen ? (
             <RxCross1
-              className={`ml-auto text-2xl ${
+              className={`ml-auto text-2xl lg:hidden ${
                 transparentNav || isHamburgerMenuOpen ? 'text-white' : ''
               }`}
               onClick={() => setIsHamburgerMenuOpen(!isHamburgerMenuOpen)}
             />
           ) : (
             <RxHamburgerMenu
-              className={`ml-auto text-2xl ${
+              className={`ml-auto text-2xl lg:hidden ${
                 transparentNav ? 'text-white' : ''
               }`}
               onClick={() => setIsHamburgerMenuOpen(!isHamburgerMenuOpen)}
@@ -92,6 +152,8 @@ const NavBar = ({ transparentNav }) => {
           )}
         </div>
       </header>
+
+      {/* Hamburger menu */}
       {isHamburgerMenuOpen && <HamburgerMenu />}
     </>
   );
