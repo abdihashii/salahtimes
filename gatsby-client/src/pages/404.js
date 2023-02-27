f95f6447a5d88eac61ae379773cad8d4709ad6bc
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-02-27 12:56:39 -0600

    styling for mobile 404

--- gatsby-client/src/pages/404.js
+++ gatsby-client/src/pages/404.js
@@ -3,25 +3,17 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
-
 const NotFoundPage = () => {
   return (
     <Layout>
       <main className="mx-auto w-10/12 text-center lg:w-5/12 lg:pb-10">
         <h1
-          className="mt-10 mb-22px font-semibold tracking-widest
+          className="mt-10 mb-7 text-8xl font-semibold tracking-widest
         text-green-dark lg:mt-6 lg:mb-30px lg:pt-16 lg:text-9xl"
         >
           404
         </h1>
-        <h2 className="text-xl lg:mb-14">Oops!... Page not found!</h2>
+        <h2 className="mb-14 text-xl">Oops!... Page not found!</h2>
 
         <Link
           className="rounded-full bg-green-dark py-4 px-12 font-semibold uppercase text-white hover:bg-green-secondary"
