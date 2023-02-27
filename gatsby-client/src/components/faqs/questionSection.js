ed62f0434581e24f1f26c5efacf3c933b7d3f00a
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-02-27 14:57:57 -0600

    Fixing spacing for faqs

--- gatsby-client/src/components/faqs/questionSection.js
+++ gatsby-client/src/components/faqs/questionSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 const QuestionSection = ({ title, className, children }) => {
   return (
     <section
-      className={`mx-auto mb-28 w-10/12 last:mb-0 lg:w-5/12 ${className}`}
+      className={`mx-auto mb-16 w-10/12 last:mb-0 lg:w-5/12 ${className}`}
     >
       <h2 className="mb-6 text-3xl font-bold text-green-dark lg:text-5xl">
         {title}
