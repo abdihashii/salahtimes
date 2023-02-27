fa4d5671dc9d6341b71a5c69a0c65f0da6fffe22
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-02-27 13:03:45 -0600

    Screens/404 (#39)
    * adding desktop 404 screen stylings
    
    * styling for mobile 404

--- gatsby-client/src/pages/404.js
+++ gatsby-client/src/pages/404.js
@@ -1,49 +1,31 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <Layout>
+      <main className="mx-auto w-10/12 text-center lg:w-5/12 lg:pb-10">
+        <h1
+          className="mt-10 mb-7 text-8xl font-semibold tracking-widest
+        text-green-dark lg:mt-6 lg:mb-30px lg:pt-16 lg:text-9xl"
+        >
+          404
+        </h1>
+        <h2 className="mb-14 text-xl">Oops!... Page not found!</h2>
 
-export default NotFoundPage
+        <Link
+          className="rounded-full bg-green-dark py-4 px-12 font-semibold uppercase text-white hover:bg-green-secondary"
+          to="/"
+        >
+          Go back home
+        </Link>
+      </main>
+    </Layout>
+  );
+};
 
-export const Head = () => <title>Not found</title>
+export default NotFoundPage;
+
+export const Head = () => <Seo pageTitle="404 - Page Not Found" />;
