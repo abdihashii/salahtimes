efe4905f36c3ad1d2e8f68b77e1b2e74a618e05f
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-01 13:29:40 -0600

    fixing footer spacing

--- gatsby-client/src/components/layout/Footer/index.js
+++ gatsby-client/src/components/layout/Footer/index.js
@@ -17,8 +17,8 @@ const Footer = () => {
     <footer className="mx-auto mt-56px flex flex-col border-t-3px border-green-secondary pt-12 tracking-0.011em">
       <section className="lg:mx-auto lg:flex lg:w-8/12 lg:flex-row lg:gap-16 xl:w-7/12">
         {/* Logo and One-liner */}
-        <div className="lg:flex lg:flex-col">
-          {/* Logo */}
+        <div className="lg:flex lg:w-4/12 lg:flex-col">
+          {/* Logo and logo name */}
           <div className="mb-8 flex flex-row items-center justify-center lg:mb-36px lg:justify-start">
             <GatsbyImage
               image={faviconCropped.childImageSharp.gatsbyImageData}
@@ -39,27 +39,28 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Prayer times, blog, about us, contact us, and faqs */}
-        <ul className="mb-50px flex flex-col text-center text-lg font-medium leading-44px text-text-grey lg:whitespace-nowrap lg:text-left">
-          <li>
-            <Link
-              to="/"
-              className="opacity-50 hover:opacity-100"
-              activeStyle={{ opacity: 1 }}
-            >
-              Prayer Times
-            </Link>
-          </li>
-          <li className="inline-block">
-            <Link
-              to="/hijri-date/"
-              className="opacity-50 hover:opacity-100"
-              activeStyle={{ opacity: 1 }}
-            >
-              Hijri date
-            </Link>
-          </li>
-          {/* <li className="inline-block">
+        <div className="lg:flex lg:w-8/12 lg:flex-row lg:justify-between">
+          {/* Prayer times, blog, about us, contact us, and faqs */}
+          <ul className="mb-50px flex flex-col text-center text-lg font-medium leading-44px text-text-grey lg:whitespace-nowrap lg:text-left">
+            <li>
+              <Link
+                to="/"
+                className="opacity-50 hover:opacity-100"
+                activeStyle={{ opacity: 1 }}
+              >
+                Prayer Times
+              </Link>
+            </li>
+            <li className="inline-block">
+              <Link
+                to="/hijri-date/"
+                className="opacity-50 hover:opacity-100"
+                activeStyle={{ opacity: 1 }}
+              >
+                Hijri date
+              </Link>
+            </li>
+            {/* <li className="inline-block">
             <Link
               to="/ramadan-2023/"
               className="opacity-50 hover:opacity-100"
@@ -68,81 +69,81 @@ const Footer = () => {
               Ramadan 2023
             </Link>
           </li> */}
-          <li>
-            <Link
-              to="/blog/"
-              className="opacity-50 hover:opacity-100"
-              activeStyle={{ opacity: 1 }}
-            >
-              Blog
-            </Link>
-          </li>
-        </ul>
+            <li>
+              <Link
+                to="/blog/"
+                className="opacity-50 hover:opacity-100"
+                activeStyle={{ opacity: 1 }}
+              >
+                Blog
+              </Link>
+            </li>
+          </ul>
 
-        {/* Info links */}
-        <ul className="mb-55px flex flex-col text-center text-lg font-medium leading-44px text-text-grey lg:whitespace-nowrap lg:text-left">
-          <li>
-            <Link
-              to="/about-us/"
-              className="opacity-50 hover:opacity-100"
-              activeStyle={{ opacity: 1 }}
-            >
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/faqs/"
-              className="opacity-50 hover:opacity-100"
-              activeStyle={{ opacity: 1 }}
-            >
-              FAQs
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact-us/"
-              className="opacity-50 hover:opacity-100"
-              activeStyle={{ opacity: 1 }}
-            >
-              Contact Us
-            </Link>
-          </li>
-        </ul>
+          {/* Info links */}
+          <ul className="mb-55px flex flex-col text-center text-lg font-medium leading-44px text-text-grey lg:whitespace-nowrap lg:text-left">
+            <li>
+              <Link
+                to="/about-us/"
+                className="opacity-50 hover:opacity-100"
+                activeStyle={{ opacity: 1 }}
+              >
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/faqs/"
+                className="opacity-50 hover:opacity-100"
+                activeStyle={{ opacity: 1 }}
+              >
+                FAQs
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact-us/"
+                className="opacity-50 hover:opacity-100"
+                activeStyle={{ opacity: 1 }}
+              >
+                Contact Us
+              </Link>
+            </li>
+          </ul>
 
-        {/* Privacy policy, terms of use, disclamer, and help center */}
-        <ul className="mb-50px flex flex-col text-center text-lg font-medium leading-44px text-text-grey lg:whitespace-nowrap lg:text-left">
-          <li className="inline-block">
-            <Link
-              to="/privacy-policy"
-              className="opacity-50 hover:opacity-100"
-              activeStyle={{ opacity: 1 }}
-            >
-              Privacy Policy
-            </Link>
-          </li>
-          <li className="inline-block">
-            <Link
-              to="/cookies-policy"
-              className="opacity-50 hover:opacity-100"
-              activeStyle={{ opacity: 1 }}
-            >
-              Cookies Policy
-            </Link>
-          </li>
-          {/* <li className="inline-block">
+          {/* Privacy policy, terms of use, disclamer, and help center */}
+          <ul className="mb-50px flex flex-col text-center text-lg font-medium leading-44px text-text-grey lg:whitespace-nowrap lg:text-left">
+            <li className="inline-block">
+              <Link
+                to="/privacy-policy"
+                className="opacity-50 hover:opacity-100"
+                activeStyle={{ opacity: 1 }}
+              >
+                Privacy Policy
+              </Link>
+            </li>
+            <li className="inline-block">
+              <Link
+                to="/cookies-policy"
+                className="opacity-50 hover:opacity-100"
+                activeStyle={{ opacity: 1 }}
+              >
+                Cookies Policy
+              </Link>
+            </li>
+            {/* <li className="inline-block">
             <Link to="/">Terms of Use</Link>
           </li> */}
-          {/* <li className="inline-block">
+            {/* <li className="inline-block">
             <Link to="/">Disclamer</Link>
           </li> */}
-          {/* <li className="inline-block">
+            {/* <li className="inline-block">
             <Link to="/">Help Center</Link>
           </li> */}
-        </ul>
+          </ul>
 
-        {/* Manage preferences and cookies policy */}
-        {/* <ul className="mb-55px flex flex-col text-center text-lg font-medium leading-44px text-text-grey lg:whitespace-nowrap lg:text-left">
+          {/* Manage preferences and cookies policy */}
+          {/* <ul className="mb-55px flex flex-col text-center text-lg font-medium leading-44px text-text-grey lg:whitespace-nowrap lg:text-left">
           <li className="inline-block">
             <Link to="/">Manage Preferences</Link>
           </li>
@@ -157,22 +158,23 @@ const Footer = () => {
           </li>
         </ul> */}
 
-        {/* Social */}
-        <div>
-          {/* Follow us */}
-          <p className="mb-26px flex justify-center text-xl font-medium uppercase leading-39px text-text-layout_text lg:justify-start">
-            Follow Us
-          </p>
+          {/* Social */}
+          <div>
+            {/* Follow us */}
+            <p className="mb-26px flex justify-center text-xl font-medium uppercase leading-39px text-text-layout_text lg:justify-start">
+              Follow Us
+            </p>
 
-          {/* Social media links */}
-          <ul className="mb-63px flex flex-row justify-center gap-x-11px">
-            <li>
-              <a href="/">Twitter</a>
-            </li>
-            <li>
-              <a href="/">Instagram</a>
-            </li>
-          </ul>
+            {/* Social media links */}
+            <ul className="mb-63px flex flex-row justify-center gap-x-11px">
+              <li>
+                <a href="/">Twitter</a>
+              </li>
+              <li>
+                <a href="/">Instagram</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
 
