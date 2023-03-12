86c044b31a5eb412aaf8e2cc5c26173dbbb169aa
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-11 19:50:24 -0600

    trying htacess plugin to see if this helps with redirects

--- gatsby-client/gatsby-config.js
+++ gatsby-client/gatsby-config.js
@@ -11,13 +11,6 @@ module.exports = {
     siteUrl: `https://www.myprayertimes.com`,
   },
   plugins: [
-    // {
-    //   resolve: 'gatsby-plugin-google-analytics',
-    //   options: {
-    //     trackingId: process.env.GOOGLE_ANALYTICS_PROPERTY_ID,
-    //     head: true,
-    //   },
-    // },
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
@@ -46,33 +39,24 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-htaccess',
+      options: {
+        https: true,
+        www: true,
+        host: 'www.myprayertimes.com', // if 'www' is set to 'false', be sure to also remove it here!
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           process.env.GOOGLE_ANALYTICS_PROPERTY_ID, // Google Analytics / GA
-          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: 'OPT_CONTAINER_ID',
-        //   anonymize_ip: true,
-        //   cookie_expires: 0,
-        // },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
-          // respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          // exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          // origin: "YOUR_SELF_HOSTED_ORIGIN",
-          // Delays processing pageview events on route update (in milliseconds)
-          // delayOnRouteUpdate: 0,
         },
       },
     },
@@ -111,13 +95,6 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-        purgeCSSOptions: {
-          // https://purgecss.com/configuration.html#options
-          // safelist: ['safelist'], // Don't remove this selector
-        },
-        // More options defined here https://purgecss.com/configuration.html#options
       },
     },
     {
