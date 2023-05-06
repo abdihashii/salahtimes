2a3274f894010f0e67eec5ef9fea60238b5845b7
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-05-06 15:55:19 -0500

    plus one to hijri date

--- gatsby-client/src/hooks/useHijriDate.js
+++ gatsby-client/src/hooks/useHijriDate.js
@@ -4,7 +4,7 @@ import { getTodaysDate } from '../controllers/prayerTimesController';
 const useHijriDate = () => {
   const [hijriDate, setHijriDate] = useState({});
   const { forHijri, gregorianDate } = getTodaysDate();
-  const adjustment = 0;
+  const adjustment = 1;
 
   useEffect(() => {
     const fetchHijriDate = async () => {
