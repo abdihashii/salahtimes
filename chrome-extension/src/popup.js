99195f6a652ecc1417afa3b14cc0bb1cfb8715d6
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-07-17 16:08:54 -0500

    adds loading spinner on click and a lot of other style changes

--- chrome-extension/src/popup.js
+++ chrome-extension/src/popup.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   var prayerTimesButton = document.getElementById('prayerTimesButton');
+  var loadingSpinner = document.getElementById('loadingSpinner');
 
   prayerTimesButton.addEventListener('click', getPrayerTimes);
 
   function getPrayerTimes() {
-    alert('Button clicked. Getting prayer times...');
+    prayerTimesButton.classList.add('hidden'); // hide button
+    loadingSpinner.classList.remove('hidden'); // show loading spinner
+
+    // hide loading spinner after 3 seconds for demo
+    setTimeout(function () {
+      loadingSpinner.classList.add('hidden');
+      prayerTimesButton.classList.remove('hidden');
+    }, 3000);
   }
 });
