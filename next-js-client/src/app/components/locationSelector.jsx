d8e901326dd7b5aebe39f8ab98a628d743fa6eb5
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-07-16 23:04:52 -0500

    Adds icons to location selector

--- next-js-client/src/app/components/locationSelector.jsx
+++ next-js-client/src/app/components/locationSelector.jsx
@@ -13,6 +13,8 @@ import {
   getPrayerTimesFromAPI,
 } from '../utils/prayerTimes';
 import useGoogle from 'react-google-autocomplete/lib/usePlacesAutocompleteService';
+import LoadingIcon from '../icons/loadingIcon';
+import LocationIcon from '../icons/locationIcon';
 
 const LocationSelector = () => {
   const [input, setInput] = useAtom(inputAtom);
@@ -70,17 +72,36 @@ const LocationSelector = () => {
   });
 
   return (
-    <form className="mb-8 flex flex-col items-center justify-center">
-      <input
-        className="rounded-4px mb-1 h-14 w-1/3 border border-gray-300 px-5 text-black focus:border-green-700 focus:outline-none"
-        value={input.city_name}
-        placeholder="Debounce 500 ms"
-        onChange={(evt) => {
-          getPlacePredictions({ input: evt.target.value });
-          setInput({ city_name: evt.target.value });
-        }}
-        // loading={isPlacePredictionsLoading}
-      />
+    <form className="mb-8 mt-4 flex flex-col items-center justify-center sm:mt-8 lg:mt-16">
+      <div className="mb-4 flex w-full flex-row gap-2 lg:w-3/4 lg:justify-between">
+        <input
+          className="h-14 w-full rounded-lg border border-gray-300 px-5 text-black focus:border-green-700 focus:outline-none sm:mb-6 sm:w-1/3 lg:mb-8 lg:w-10/12"
+          value={input.city_name}
+          placeholder="Debounce 500 ms"
+          onChange={(evt) => {
+            getPlacePredictions({ input: evt.target.value });
+            setInput({ city_name: evt.target.value });
+          }}
+        />
+
+        {/* Get location lat and long */}
+        <button
+          type="button"
+          className={`flex h-14 w-24 transform flex-row items-center justify-center rounded-lg bg-indigo-600 px-5 font-semibold text-white transition-all duration-500 ease-in-out hover:scale-105 ${
+            locationLoading ? 'cursor-not-allowed' : 'hover:bg-indigo-500'
+          }`}
+          onClick={handleGetLocation} // Set onClick handler here
+          disabled={locationLoading} // Disable the button when loading
+        >
+          {!locationLoading ? (
+            <LoadingIcon />
+          ) : (
+            <div className="mx-auto inline-block h-5 w-5 animate-spin">
+              <LocationIcon />
+            </div>
+          )}
+        </button>
+      </div>
 
       {/* Place predictions */}
       {placePredictions.length > 0 && (
@@ -102,70 +123,12 @@ const LocationSelector = () => {
       )}
 
       {/* Buttons that get prayer times and current location */}
-      <div className="mb-5 flex w-1/3 flex-row gap-3">
+      <div className="mt-4 flex flex-col items-center justify-center gap-4 sm:mt-8 sm:flex-row lg:mt-16">
         <button
           type="submit"
-          className="gap-10px mt-5 flex h-14 w-3/4 flex-row items-center justify-center rounded-md bg-green-800 px-5 py-2 font-semibold text-white transition-colors duration-200 ease-in-out hover:bg-green-700"
+          className="mb-2 flex h-12 w-full transform flex-row items-center justify-center rounded-lg bg-green-600 px-5 font-semibold text-white transition-all duration-500 ease-in-out hover:scale-105 hover:bg-green-500 sm:mb-0 sm:w-64"
         >
           My Prayer Times
-        </button>
-
-        {/* Get location lat and long */}
-        <button
-          type="button"
-          className={`gap-10px mt-5 flex h-14 w-1/4 flex-row items-center justify-center rounded-md bg-green-800 px-5 py-2 font-semibold text-white ${
-            locationLoading
-              ? 'cursor-not-allowed'
-              : 'transition-colors duration-200 ease-in-out hover:bg-green-700'
-          }`}
-          onClick={handleGetLocation} // Set onClick handler here
-          disabled={locationLoading} // Disable the button when loading
-        >
-          {!locationLoading ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 2C8.134 2 5 5.134 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.866-3.134-7-7-7z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 11a2 2 0 100-4 2 2 0 000 4z"
-              />
-            </svg>
-          ) : (
-            <div className="mx-auto inline-block h-5 w-5 animate-spin">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="stroke-current text-white"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            </div>
-          )}
         </button>
       </div>
 
---
+++ next-js-client/src/app/icons/loadingIcon.jsx
@@ -0,0 +1,26 @@
+const LoadingIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 2C8.134 2 5 5.134 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.866-3.134-7-7-7z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 11a2 2 0 100-4 2 2 0 000 4z"
+      />
+    </svg>
+  );
+};
+
+export default LoadingIcon;
