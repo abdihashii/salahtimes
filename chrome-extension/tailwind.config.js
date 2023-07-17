66831db92374eb450cd8f93b64929befca914193
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-07-17 16:08:16 -0500

    adds extensions to tailwind config

--- chrome-extension/tailwind.config.js
+++ chrome-extension/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          main: '#276749', // the main brand color
+          secondary: '#672745', // dark purple
+        },
+        'soft-gray': '#F7FAFC', // a very light, less intensive gray color
+      },
+      borderRadius: {
+        xl: '1rem',
+      },
+    },
   },
+  variants: {},
   plugins: [],
 };
