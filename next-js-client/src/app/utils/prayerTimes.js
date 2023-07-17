b4d19fe7c8cae2a8aabd87e252e6698b95fa8324
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-07-16 22:18:59 -0500

    creates prayer times utils fiel

---
+++ next-js-client/src/app/utils/prayerTimes.js
@@ -0,0 +1,49 @@
+/**
+ * @returns {string} Current day in the format of "DD-MM-YYYY"
+ */
+export const getCurrentDay = () => {
+  const dateObj = new Date();
+  const day = dateObj.getDate();
+  const month = dateObj.getMonth() + 1;
+  const year = dateObj.getFullYear();
+
+  return `${day}-${month}-${year}`;
+};
+
+/**
+ * @returns {Promise<GeolocationPosition>} GeolocationPosition
+ * @throws {Error} Error
+ */
+export const getUserGeolocation = async () => {
+  if (!navigator.geolocation) {
+    throw new Error('Geolocation is not supported by this browser.');
+  }
+
+  const position =
+    (await new Promise()) <
+    GeolocationPosition >
+    ((resolve, reject) =>
+      navigator.geolocation.getCurrentPosition(resolve, reject));
+
+  const { latitude, longitude } = position.coords;
+
+  return { lat: latitude, lng: longitude };
+};
+
+/**
+ * @param {Object} coords
+ * @param {number} coords.lat
+ * @param {number} coords.lng
+ * @returns {Promise<Object>} Prayer times
+ * @throws {Error} Error
+ */
+export const getPrayerTimesFromAPI = async (coords) => {
+  const { lat, lng } = coords;
+  const day = getCurrentDay();
+
+  const response = await fetch(
+    `http://api.aladhan.com/v1/timings/${day}?latitude=${lat}&longitude=${lng}&method=2`
+  );
+  const data = await response.json();
+  return data.data.timings;
+};
