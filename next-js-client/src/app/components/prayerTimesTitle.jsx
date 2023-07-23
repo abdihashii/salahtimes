56aa543e36456cf447cecc3a12035a7a5d491cff
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-07-23 16:48:29 +0000

    uses input.label in title

--- next-js-client/src/app/components/prayerTimesTitle.jsx
+++ next-js-client/src/app/components/prayerTimesTitle.jsx
@@ -5,13 +5,13 @@ import { inputAtom, prayerTimesAtom } from '../atoms/prayerTimesAtoms';
 
 const PrayerTimesTitle = () => {
   const [input] = useAtom(inputAtom);
-  const [prayerTimes] = useAtom(prayerTimesAtom);
+  // const [prayerTimes] = useAtom(prayerTimesAtom);
 
   return (
     <>
       <h1 className="mx-auto my-10 w-full text-4xl font-medium text-indigo-900 sm:my-20 sm:w-10/12 sm:text-6xl">
         Prayer times in "
-        <span className="text-indigo-700">{input.city_name}</span>"
+        <span className="text-indigo-700">{input.label}</span>"
       </h1>
       {/* <pre className="rounded-4px mx-auto mb-20 w-1/4 bg-black p-5 text-white">
         {JSON.stringify(input, null, 2)}
