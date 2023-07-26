a5602a280aaf1c69e92bf7a4f4e32a08011bd2b0
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-07-26 18:27:31 -0500

    uses new adhan library

--- prayer-times-api/utils.test.ts
+++ prayer-times-api/utils.test.ts
@@ -1,78 +1,38 @@
-import { gregorianToJulianDay, getDayOfYear, getDeclination, computeTime, getEquationOfTime } from './utils';
-import {PrayTimes} from './prayerTimes'
+import { CalculationMethod } from 'adhan';
+import { getPrayerTimes } from './utils';
 
-const PT = new (PrayTimes as any)('ISNA');
-const date = new Date();
-const timeZone = PT.getTimeZone(date);
-console.log(`timeZone: ${timeZone}`);
-// var times = PT.getTimes(new Date(), [41.760586, -88.320068], -5);
+describe('Utility Functions', () => {
+  // Testing prayer times for July 26, 2023 in Aurora, IL
+  test('getPrayerTimes should return the correct prayer times for July 26, 2023 in Aurora, IL', () => {
+    const date = new Date(2023, 6, 26);
+    const lat = 41.7605849;
+    const lng = -88.3200715;
+    const calcMethod = CalculationMethod.MuslimWorldLeague();
 
+    const prayerTimes = getPrayerTimes(lat, lng, date, calcMethod);
 
-// const julianDay = gregorianToJulianDay(2006, 6, 15)
-// console.log(`julianDay: ${julianDay}`);
-// const diff = julianDay - 2453901.500000;
-// console.log(`diff: ${diff}`);
+    expect(prayerTimes.fajr).toBe('3:44 AM');
+    expect(prayerTimes.sunrise).toBe('5:41 AM');
+    expect(prayerTimes.dhuhr).toBe('1:01 PM');
+    expect(prayerTimes.asr).toBe('4:57 PM');
+    expect(prayerTimes.maghrib).toBe('8:18 PM');
+    expect(prayerTimes.isha).toBe('10:07 PM');
+  });
 
-// describe('Utility Functions', () => {
+  // Testing prayer times for July 26, 2023 in Jebel Ali Village, Dubai, UAE
+  // test('getPrayerTimes should return the correct prayer times for July 26, 2023 in Jebel Ali Village, Dubai, UAE', () => {
+  //   const date = new Date(2023, 6, 26);
+  //   const lat = 25.038015;
+  //   const lng = 55.11755;
+  //   const calcMethod = CalculationMethod.MoonsightingCommittee();
 
-//     describe('dateToJulianDay', () => {
+  //   const prayerTimes = getPrayerTimes(lat, lng, date, calcMethod);
 
-//         // Basic Tests
-//         it('should correctly convert a date to its Julian Day representation', () => {
-//             expect(dateToJulianDay(2006, 6, 15)).toBeCloseTo(2453901.5, 3);
-//             expect(dateToJulianDay(2023, 5, 21)).toBeCloseTo(2457442.5, 3);
-//             expect(dateToJulianDay(2009, 6, 12)).toBeCloseTo(2454994.5, 3);
-//         });
-
-//         // Edge Cases
-//         it('should handle dates before the Gregorian calendar reform', () => {
-//             expect(dateToJulianDay(1582, 10, 4)).toBeCloseTo(2299160.5, 3);   // Last day of Julian calendar
-//             expect(dateToJulianDay(1582, 10, 15)).toBeCloseTo(2299161.5, 3); // First day of Gregorian calendar
-//             expect(dateToJulianDay(1000, 1, 1)).toBeCloseTo(2086308.5, 3);
-//         });
-
-//         it('should handle edge months and leap years', () => {
-//             expect(dateToJulianDay(2000, 2, 28)).toBeCloseTo(2451603.5, 3);  // Leap year
-//             expect(dateToJulianDay(2000, 2, 29)).toBeCloseTo(2451604.5, 3);  // Leap day
-//             expect(dateToJulianDay(2001, 2, 28)).toBeCloseTo(2451968.5, 3);  // Non-leap year
-
-//             expect(dateToJulianDay(2000, 1, 1)).toBeCloseTo(2451545.5, 3);  // Start of the year
-//             expect(dateToJulianDay(2000, 12, 31)).toBeCloseTo(2451910.5, 3);  // End of the year
-//         });
-
-//     });
-
-//     // Testing getDayOfYear
-//     test('getDayOfYear should return the correct day of the year', () => {
-//         expect(getDayOfYear(new Date(2023, 0, 1))).toBe(1);  // January 1
-//         expect(getDayOfYear(new Date(2023, 11, 31))).toBe(365);  // December 31
-//         expect(getDayOfYear(new Date(2024, 1, 29))).toBe(60);  // Leap Year: Feb 29
-//     });
-
-//     // Testing getDeclination
-//     test('getDeclination should return the approximate declination angle', () => {
-//         expect(getDeclination(new Date(2023, 5, 21))).toBeCloseTo(23.44, 1);  // Around June solstice
-//         expect(getDeclination(new Date(2023, 11, 21))).toBeCloseTo(-23.44, 1);  // Around December solstice
-//         expect(getDeclination(new Date(2023, 2, 20, 12, 0, 0))).toBeCloseTo(0, 1);  // Around March equinox
-//         expect(getDeclination(new Date(2023, 8, 22))).toBeCloseTo(0, 1);  // Around September equinox
-//     });
-
-//     // Testing computeTime
-//     test('computeTime should compute time correctly', () => {
-//         // Just a representative test; in reality, you'd need more specific known values to validate
-//         const declination = 23.44;  // Approx for solstice
-//         const latitude = 52;       // Roughly for London
-//         expect(computeTime(-0.833, declination, latitude)).toBeGreaterThan(0);
-//         expect(computeTime(-0.833, declination, latitude)).toBeLessThan(24);
-//     });
-
-//     // Testing getEquationOfTime
-//     test('getEquationOfTime should return values within expected range', () => {
-//         for (let month = 0; month < 12; month++) {
-//             const eot = getEquationOfTime(new Date(2023, month, 15));
-//             expect(eot).toBeGreaterThanOrEqual(-14);
-//             expect(eot).toBeLessThanOrEqual(16);
-//         }
-//     });
-
-// });
+  // expect(prayerTimes.fajr).toBe('4:19 AM');
+  // expect(prayerTimes.sunrise).toBe('5:44 AM');
+  // expect(prayerTimes.dhuhr).toBe('12:26 PM');
+  // expect(prayerTimes.asr).toBe('3:53 PM');
+  // expect(prayerTimes.maghrib).toBe('7:08 PM');
+  // expect(prayerTimes.isha).toBe('8:28 PM');
+  // });
+});
--- prayer-times-api/utils.ts
+++ prayer-times-api/utils.ts
@@ -1,87 +1,49 @@
-/**
- * Converts a Gregorian date to Julian Day Number.
- * 
- * @param year - The year of the date.
- * @param month - The month of the date (1 for January, 2 for February, etc.)
- * @param day - The day of the month.
- * @returns The Julian Day Number.
- */
-function gregorianToJulianDay(year: number, month: number, day: number): number {
-    const Y = year;
-    const M = month + 1; // Months are zero-indexed in JS
-    const D = day;
+import {
+  Coordinates,
+  PrayerTimes,
+  CalculationMethod,
+  CalculationParameters,
+} from 'adhan';
+// import { format, utcToZonedTime, zonedTimeToUtc } from 'date-fns-tz';
+import { format } from 'date-fns';
 
-    // // Break down the formula into parts for clarity
-    // const part1 = (1461 * (Y + 4800 + (M - 14) / 12)) / 4;
-    // const part2 = (367 * (M - 2 - 12 * ((M - 14) / 12))) / 12;
-    // const part3 = (3 * ((Y + 4900 + (M - 14) / 12) / 100)) / 4;
+const getPrayerTimes = (
+  lat: number,
+  lng: number,
+  date: Date,
+  calcMethod: CalculationParameters,
+) => {
+  const coordinates = new Coordinates(lat, lng);
+  const prayerTimes = new PrayerTimes(coordinates, date, calcMethod);
 
-    // // Combine parts according to the formula
-    // const JDN = part1 + part2 - part3 + D - 32075;
+  // console.log(prayerTimes);
 
-    // return Math.floor(JDN);  // Ensure we return an integer value
+  // const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  // const zonedDate = zonedTimeToUtc(date, 'Asia/Dubai');
 
-    // return Math.floor((1461 * (Y + 4800 + (M - 14) / 12)) / 4 + (367 * (M - 2 - 12 * ((M - 14) / 12))) / 12 - (3 * ((Y + 4900 + (M - 14) / 12) / 100)) / 4 + D - 32075);
+  // const dt = new Date(date.valueOf() + date.getTimezoneOffset() * 60 * 1000);
+  // const formatted = format(dt, 'yyyy-MM-dd HH:mm:ss zzz');
+  // console.log(`dt: ${dt}`);
+  // console.log(`formatted: ${formatted}`);
 
-    return (1461 * (Y + 4800 + (M - 14)/12))/4 +(367 * (M - 2 - 12 * ((M - 14)/12)))/12 - (3 * ((Y + 4900 + (M - 14)/12)/100))/4 + D - 32075
-}
+  const fajr = format(prayerTimes.fajr, 'h:mm a');
+  // // console.log(`fajr: ${fajr}`);
+  // // console.log(`utcZonedTime: ${zonedDate}`);
 
-/**
- * Get the day of the year for a given date.
- * @param {Date} date The date for which to determine the day of the year.
- * @returns {number} The day of the year.
- */
-function getDayOfYear(date: Date): number {
-    const start = new Date(date.getFullYear(), 0, 0);
-    const diff = (date.valueOf() - start.valueOf()) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
-    const oneDay = 1000 * 60 * 60 * 24;
-    const day = Math.floor(diff / oneDay);
-    return day;
-}
+  const sunrise = format(prayerTimes.sunrise, 'h:mm a');
+  const dhuhr = format(prayerTimes.dhuhr, 'h:mm a');
+  const asr = format(prayerTimes.asr, 'h:mm a');
+  const maghrib = format(prayerTimes.maghrib, 'h:mm a');
+  const isha = format(prayerTimes.isha, 'h:mm a');
 
-/**
- * Get the solar declination angle for a given day.
- * @param {Date} date The date for which to calculate the declination.
- * @returns {number} The declination angle in degrees.
- */
-function getDeclination(date: Date): number {
-    const n = getDayOfYear(date);
-    const radianFactor = 2 * Math.PI / 365.24;
-    const declination = 23.44 * Math.sin(radianFactor * (n + 10) + 1.914 * Math.sin(radianFactor * n));
-    return declination;
-}
+  return {
+    fajr,
+    sunrise,
+    dhuhr,
+    asr,
+    maghrib,
+    isha,
+  };
+};
 
-/**
- * Compute the time for a specific solar angle.
- * @param {number} angle The solar angle.
- * @param {number} declination The declination angle of the sun.
- * @param {number} latitude The latitude of the location.
- * @returns {number} The time as hours since midnight.
- */
-function computeTime(angle: number, declination: number, latitude: number): number {
-    // Convert degrees to radians
-    declination = declination * (Math.PI / 180);
-    latitude = latitude * (Math.PI / 180);
-
-    // Compute the hour angle
-    const cosHourAngle = (Math.sin(angle) - Math.sin(latitude) * Math.sin(declination)) / (Math.cos(latitude) * Math.cos(declination));
-
-    // Ensure value is in the range [-1, 1] to avoid NaN
-    const boundedCosHourAngle = Math.max(-1, Math.min(1, cosHourAngle));
-
-    return (1 / 15) * Math.acos(boundedCosHourAngle);
-}
-
-
-/**
- * Get the equation of time for a given day.
- * @param {Date} date The date for which to calculate the equation of time.
- * @returns {number} The equation of time in minutes.
- */
-function getEquationOfTime(date: Date): number {
-    const n = getDayOfYear(date);
-    const eot = 229.18 * (0.000075 + 0.001868 * Math.cos(n) - 0.032077 * Math.sin(n) - 0.014615 * Math.cos(2 * n) - 0.040849 * Math.sin(2 * n));
-    return Math.max(-14, Math.min(16, eot));
-}
-
-export { gregorianToJulianDay, getDayOfYear, getDeclination, computeTime, getEquationOfTime };
+export { getPrayerTimes };
