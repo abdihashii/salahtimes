38ec24952eca1f32aa655801027873f495ffe0a7
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-08-01 17:39:08 -0500

    Updates API to use raw and formatted data in the response

--- prayer-times-api/src/index.ts
+++ prayer-times-api/src/index.ts
@@ -1,8 +1,37 @@
-import { CalculationMethod } from "adhan";
-import { getPrayerTimes } from "./utils";
+import { CalculationMethod } from 'adhan';
+import { getPrayerTimes } from './utils';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  const name = req.body.name || 'World';
+
+  res.send(`Hello, ${name}!`);
+});
+
+app.get('/get-prayer-times', (req, res) => {
+  const date = req.body.date ? new Date(req.body.date) : new Date();
+  const lat = req.body.lat;
+  const lng = req.body.lng;
+  const calcMethod = req.body.method
+    ? req.body.method
+    : CalculationMethod.MuslimWorldLeague();
+
+  const prayerTimes = getPrayerTimes(lat, lng, date, calcMethod);
+
+  res.send(prayerTimes);
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000!');
+});
 
 const main = () => {
-  console.log("hello world");
+  console.log('hello world');
 
   const date = new Date(2023, 6, 26);
   const lat = 41.7605849;
@@ -14,4 +43,4 @@ const main = () => {
   console.log(prayerTimes);
 };
 
-main();
+// main();
--- prayer-times-api/src/utils.ts
+++ prayer-times-api/src/utils.ts
@@ -1,33 +1,36 @@
-import { Coordinates, PrayerTimes, CalculationParameters } from "adhan";
-import moment from "moment-timezone";
-import { find } from "geo-tz";
+import { Coordinates, PrayerTimes, CalculationParameters } from 'adhan';
+import moment from 'moment-timezone';
+import { find } from 'geo-tz';
 
 const getPrayerTimes = (
   lat: number,
   lng: number,
   date: Date,
-  calcMethod: CalculationParameters
+  calcMethod: CalculationParameters,
 ) => {
   const coordinates = new Coordinates(lat, lng);
   const prayerTimes = new PrayerTimes(coordinates, date, calcMethod);
 
   const timeZones = find(lat, lng); // string[]
 
-  const fajr = moment(prayerTimes.fajr).tz(timeZones[0]).format("h:mm A");
-  const sunrise = moment(prayerTimes.sunrise).tz(timeZones[0]).format("h:mm A");
-  const dhuhr = moment(prayerTimes.dhuhr).tz(timeZones[0]).format("h:mm A");
-  const asr = moment(prayerTimes.asr).tz(timeZones[0]).format("h:mm A");
-  const maghrib = moment(prayerTimes.maghrib).tz(timeZones[0]).format("h:mm A");
-  const isha = moment(prayerTimes.isha).tz(timeZones[0]).format("h:mm A");
+  const fajr = moment(prayerTimes.fajr).tz(timeZones[0]).format('h:mm A');
+  const sunrise = moment(prayerTimes.sunrise).tz(timeZones[0]).format('h:mm A');
+  const dhuhr = moment(prayerTimes.dhuhr).tz(timeZones[0]).format('h:mm A');
+  const asr = moment(prayerTimes.asr).tz(timeZones[0]).format('h:mm A');
+  const maghrib = moment(prayerTimes.maghrib).tz(timeZones[0]).format('h:mm A');
+  const isha = moment(prayerTimes.isha).tz(timeZones[0]).format('h:mm A');
 
   return {
-    fajr,
-    sunrise,
-    dhuhr,
-    asr,
-    maghrib,
-    isha,
     timeZones,
+    rawData: prayerTimes,
+    formattedData: {
+      fajr,
+      sunrise,
+      dhuhr,
+      asr,
+      maghrib,
+      isha,
+    },
   };
 };
 
