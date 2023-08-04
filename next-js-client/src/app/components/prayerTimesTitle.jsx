5a4d16411ea13a526ca699491b2a08cd89417f8f
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-08-03 19:47:03 -0500

    fix up title styling

--- next-js-client/src/app/components/prayerTimesTitle.jsx
+++ next-js-client/src/app/components/prayerTimesTitle.jsx
@@ -9,17 +9,9 @@ const PrayerTimesTitle = () => {
 
   return (
     <>
-      <h1 className="mx-auto my-10 w-full text-4xl font-medium text-indigo-900 sm:my-20 sm:w-10/12 sm:text-6xl">
-        Prayer times in "
-        <span className="text-indigo-700">{input.label}</span>"
+      <h1 className="mx-auto my-10 w-full text-5xl font-medium text-indigo-900">
+        Prayer times in "<span className="text-indigo-700">{input.label}</span>"
       </h1>
-      {/* <pre className="rounded-4px mx-auto mb-20 w-1/4 bg-black p-5 text-white">
-        {JSON.stringify(input, null, 2)}
-      </pre> */}
-
-      {/* <pre className="rounded-4px mx-auto mb-20 w-1/4 bg-black p-5 text-white">
-        {JSON.stringify(prayerTimes, null, 2)}
-      </pre> */}
     </>
   );
 };
