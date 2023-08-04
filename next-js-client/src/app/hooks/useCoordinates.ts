bef912db565c7a73ca1c90f14361e0ca9d1bb34a
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-08-03 21:41:27 -0500

    get lat lng from address using geocode maps API

--- next-js-client/src/app/hooks/useCoordinates.ts
+++ next-js-client/src/app/hooks/useCoordinates.ts
@@ -11,13 +11,30 @@ import {
   getPrayerTimesFromAPI,
 } from '../utils/prayerTimes';
 import { PrayerTimes } from '../types/prayerTimeTypes';
+import type { Input } from '../types/prayerTimeTypes';
 
 export const useCoordinates = () => {
   const [locationLoading, setLocationLoading] = useAtom(locationLoadingAtom);
   const [coordinates, setCoordinates] = useAtom(coordinatesAtom);
   const [prayerTimes, setPrayerTimes] = useAtom(prayerTimesAtom);
 
-  const handleGetLocation = async () => {
+  const handleGetLatLngFromInput = async (input: Input) => {
+    const encodedAddress =
+      input.value?.description && encodeURI(input.value.description);
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.status === 'OK') {
+      const { lat, lng } = data.results[0].geometry.location;
+      setCoordinates({ lat, lng });
+    } else {
+      console.log('Unable to retrieve your location');
+    }
+  };
+
+  const handleGetLatLngFromIPAddress = async () => {
     setLocationLoading(true);
     try {
       const coords = await getUserGeolocation();
@@ -41,7 +58,8 @@ export const useCoordinates = () => {
     locationLoading,
     coordinates,
     prayerTimes,
-    handleGetLocation,
+    handleGetLatLngFromInput,
+    handleGetLatLngFromIPAddress,
     fetchPrayerTimes,
   };
 };
