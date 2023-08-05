7ea60d7780bcbf4566f64d0fe07c2d87421fd0ca
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-08-05 14:39:33 -0500

    fixing title width

--- next-js-client/src/app/components/prayerTimesTitle.tsx
+++ next-js-client/src/app/components/prayerTimesTitle.tsx
@@ -8,7 +8,7 @@ const PrayerTimesTitle = () => {
 
   return (
     <>
-      <h1 className="mx-auto w-full text-center text-5xl font-medium">
+      <h1 className="mx-auto w-8/12 text-center text-5xl font-medium">
         Prayer times in "<span className="text-indigo-700">{input.label}</span>"
       </h1>
     </>
