f63ae75ea625c21e9169e3cf45a42de80c56758d
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-08-06 21:20:47 -0500

    Prayer times title is better styled

--- next-js-client/src/app/components/prayerTimesTitle.tsx
+++ next-js-client/src/app/components/prayerTimesTitle.tsx
@@ -8,8 +8,11 @@ const PrayerTimesTitle = () => {
 
   return (
     <>
-      <h1 className="mx-auto w-8/12 text-center text-5xl font-medium">
-        Prayer times in "<span className="text-indigo-700">{input.label}</span>"
+      <h1 className="mx-auto flex w-full flex-col gap-2 text-center text-3xl font-medium md:w-8/12 md:text-5xl">
+        <span className="text-2xl font-normal md:text-4xl">
+          Prayer times in
+        </span>
+        {input.label && <span className="text-indigo-700">{input.label}</span>}
       </h1>
     </>
   );
