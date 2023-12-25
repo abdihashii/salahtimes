41f1ba012aa95c672c52a4dbcd647b89cc1cc3d4
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-25 17:12:58 -0600

    fixing spacing in contact page to match faqs

--- next-client/src/app/contact/page.tsx
+++ next-client/src/app/contact/page.tsx
@@ -2,14 +2,16 @@ import ContactForm from '@/components/Contact/ContactForm';
 
 export default function ContactUsPage() {
   return (
-    <main className="pb-16">
-      <h1 className="mx-auto mb-6 mt-10 w-10/12 text-center text-3xl font-semibold leading-10 lg:mb-8 lg:mt-6 lg:w-5/12 lg:pt-16 lg:text-6xl">
-        Get in touch!
-      </h1>
-      <h2 className="leading-22px mx-auto mb-10 w-10/12 text-center text-sm font-normal lg:mb-16 lg:w-5/12 lg:text-lg lg:font-medium">
-        We&apos;d love to hear from you! Please fill out the form below and we
-        will get back to you as soon as possible.
-      </h2>
+    <main className="flex flex-grow flex-col gap-10 border border-red-500 py-10 lg:py-16 xl:flex-row xl:gap-0">
+      <section className="flex flex-col gap-8">
+        <h1 className="mx-auto w-10/12 text-center text-3xl font-semibold leading-10 lg:w-5/12 lg:text-6xl">
+          Get in touch!
+        </h1>
+        <h2 className="leading-22px mx-auto w-10/12 text-center text-sm font-normal lg:w-5/12 lg:text-lg lg:font-medium">
+          We&apos;d love to hear from you! Please fill out the form below and we
+          will get back to you as soon as possible.
+        </h2>
+      </section>
 
       {/* Form */}
       <ContactForm />
