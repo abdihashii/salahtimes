923543ddf5328eaf7bcbcc10af70fd3d6db5882e
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-25 17:13:32 -0600

    forgot the red border in contact page

--- next-client/src/app/contact/page.tsx
+++ next-client/src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from '@/components/Contact/ContactForm';
 
 export default function ContactUsPage() {
   return (
-    <main className="flex flex-grow flex-col gap-10 border border-red-500 py-10 lg:py-16 xl:flex-row xl:gap-0">
+    <main className="flex flex-grow flex-col gap-10 py-10 lg:py-16 xl:flex-row xl:gap-0">
       <section className="flex flex-col gap-8">
         <h1 className="mx-auto w-10/12 text-center text-3xl font-semibold leading-10 lg:w-5/12 lg:text-6xl">
           Get in touch!
