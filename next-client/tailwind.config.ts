8a1be9bdae6ea4d07377eb76349d73021759bdcc
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-30 22:21:50 -0600

    Add @tailwindcss/aspect-ratio plugin

--- next-client/tailwind.config.ts
+++ next-client/tailwind.config.ts
@@ -91,5 +91,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [
+    require('tailwindcss-animate'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
