d9506064d28510e58cf119db5d707dbae9052ae8
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-03-17 16:56:56 -0500

    Add top level loading page

---
+++ next-client/src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function LoadingAboutPage() {
+	return (
+		<main className="flex min-h-screen items-center justify-center">
+			<h1 className="text-center text-4xl font-bold text-gray-800 dark:text-white">
+				Loading...
+			</h1>
+		</main>
+	);
+}
