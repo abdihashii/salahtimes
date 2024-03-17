b9dfc15a393e6b94b4ffa459df9f971cb10879f8
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-03-17 16:55:19 -0500

    small linting

--- next-client/src/lib/utils.ts
+++ next-client/src/lib/utils.ts
@@ -2,7 +2,7 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
 }
 
 /**
@@ -12,12 +12,12 @@ export function cn(...inputs: ClassValue[]) {
  * @returns - formatted date string
  */
 export const sanitizeDate = (date: string) => {
-  const dateObj = new Date(date);
-  const month = dateObj.toLocaleString('default', { month: 'short' });
-  const day = dateObj.getDate();
-  const year = dateObj.getFullYear();
+	const dateObj = new Date(date);
+	const month = dateObj.toLocaleString('default', { month: 'short' });
+	const day = dateObj.getDate();
+	const year = dateObj.getFullYear();
 
-  return `${month} ${day}, ${year}`;
+	return `${month} ${day}, ${year}`;
 };
 
 /**
@@ -32,20 +32,20 @@ export const sanitizeDate = (date: string) => {
  * @returns - timezone id
  */
 export const fetchTimezone = async (lat: number, lng: number) => {
-  const timestamp = Math.floor(Date.now() / 1000); // Current timestamp in seconds
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
-  const url = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${timestamp}&key=${apiKey}`;
+	const timestamp = Math.floor(Date.now() / 1000); // Current timestamp in seconds
+	const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+	const url = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${timestamp}&key=${apiKey}`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    const { timeZoneId } = data;
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		const { timeZoneId } = data;
 
-    return timeZoneId;
-  } catch (error) {
-    console.error(`Error fetching timezone: ${error}`);
-    return null;
-  }
+		return timeZoneId;
+	} catch (error) {
+		console.error(`Error fetching timezone: ${error}`);
+		return null;
+	}
 };
 
 /**
@@ -55,19 +55,19 @@ export const fetchTimezone = async (lat: number, lng: number) => {
  * @returns {Promise<{lat: number, lon: number, countryCode: string, city: string}>}
  */
 export const fetchLocationFromIP = async () => {
-  try {
-    const response = await fetch(`http://ip-api.com/json`);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
+	try {
+		const response = await fetch(`http://ip-api.com/json`);
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
 
-    const data = await response.json();
+		const data = await response.json();
 
-    return data;
-  } catch (error) {
-    console.error(`Error fetching location from IP: ${error}`);
-    return null;
-  }
+		return data;
+	} catch (error) {
+		console.error(`Error fetching location from IP: ${error}`);
+		return null;
+	}
 };
 
 /**
@@ -78,20 +78,20 @@ export const fetchLocationFromIP = async () => {
  * @returns - date and time data
  */
 export const fetchDateTimeDataFromTimeZone = async (timeZone: string) => {
-  try {
-    const response = await fetch(`/api/time-api?timeZone=${timeZone}`);
+	try {
+		const response = await fetch(`/api/time-api?timeZone=${timeZone}`);
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
 
-    const data = await response.json();
+		const data = await response.json();
 
-    return data;
-  } catch (error) {
-    console.error(`Error fetching date/time data from timezone: ${error}`);
-    return null;
-  }
+		return data;
+	} catch (error) {
+		console.error(`Error fetching date/time data from timezone: ${error}`);
+		return null;
+	}
 };
 
 /**
@@ -101,8 +101,8 @@ export const fetchDateTimeDataFromTimeZone = async (timeZone: string) => {
  * @returns - time in minutes
  */
 const timeToMinutes = (time: string) => {
-  const [hours, minutes] = time.split(':').map(Number);
-  return hours * 60 + minutes;
+	const [hours, minutes] = time.split(':').map(Number);
+	return hours * 60 + minutes;
 };
 
 /**
@@ -113,50 +113,50 @@ const timeToMinutes = (time: string) => {
  * @returns - formatted time string in the format "HH:MM AM/PM"
  */
 export const formatTimeString = (timeString: string) => {
-  // Split the time string into hours and minutes
-  const [hours24, minutes] = timeString.split(':').map(Number);
+	// Split the time string into hours and minutes
+	const [hours24, minutes] = timeString.split(':').map(Number);
 
-  // Convert 24-hour time to 12-hour time
-  const hours12 = hours24 % 12 || 12;
-  const suffix = hours24 >= 12 ? 'PM' : 'AM';
+	// Convert 24-hour time to 12-hour time
+	const hours12 = hours24 % 12 || 12;
+	const suffix = hours24 >= 12 ? 'PM' : 'AM';
 
-  // Pad minutes with a leading zero if necessary
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+	// Pad minutes with a leading zero if necessary
+	const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
-  // Format and return the time string
-  return `${hours12}:${formattedMinutes} ${suffix}`;
+	// Format and return the time string
+	return `${hours12}:${formattedMinutes} ${suffix}`;
 };
 
 export const findNextPrayer = (
-  timings: {
-    [key: string]: string;
-  },
-  currentTime: string
+	timings: {
+		[key: string]: string;
+	},
+	currentTime: string,
 ) => {
-  const [currentHours, currentMinutes] = currentTime.split(':').map(Number);
-  const currentTimeInMinutes = currentHours * 60 + currentMinutes;
+	const [currentHours, currentMinutes] = currentTime.split(':').map(Number);
+	const currentTimeInMinutes = currentHours * 60 + currentMinutes;
 
-  let nextPrayer = null;
-  let smallestDiff = Number.MAX_SAFE_INTEGER;
+	let nextPrayer = null;
+	let smallestDiff = Number.MAX_SAFE_INTEGER;
 
-  for (const [prayer, time] of Object.entries(timings)) {
-    if (
-      !['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'].includes(prayer)
-    ) {
-      continue;
-    }
+	for (const [prayer, time] of Object.entries(timings)) {
+		if (
+			!['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'].includes(prayer)
+		) {
+			continue;
+		}
 
-    const prayerTimeInMinutes = timeToMinutes(time);
-    const diff = prayerTimeInMinutes - currentTimeInMinutes;
+		const prayerTimeInMinutes = timeToMinutes(time);
+		const diff = prayerTimeInMinutes - currentTimeInMinutes;
 
-    if (diff > 0 && diff < smallestDiff) {
-      smallestDiff = diff;
-      nextPrayer = {
-        prayer,
-        time: formatTimeString(time),
-      };
-    }
-  }
+		if (diff > 0 && diff < smallestDiff) {
+			smallestDiff = diff;
+			nextPrayer = {
+				prayer,
+				time: formatTimeString(time),
+			};
+		}
+	}
 
-  return nextPrayer;
+	return nextPrayer;
 };
