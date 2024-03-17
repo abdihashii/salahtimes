216e270e2c2a23b305f11c99270d854588875336
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-03-17 16:55:48 -0500

    Show blog post content using cosmicjs sdk

--- next-client/src/app/blog/[slug]/page.tsx
+++ next-client/src/app/blog/[slug]/page.tsx
@@ -1,4 +1,76 @@
-import { marked } from 'marked';
+import Image from 'next/image';
+
+import ReactMarkdown from 'react-markdown';
+import type { Components } from 'react-markdown';
+
+import { cosmic } from '@/lib/cosmicBucketClient';
+
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+
+dayjs.extend(advancedFormat);
+
+const components: Components = {
+	h2: (h2) => {
+		return (
+			<h2 className="mb-2 text-lg font-semibold text-[#0f0700]">
+				{h2.children}
+			</h2>
+		);
+	},
+	p: (p) => {
+		return (
+			<p className="mb-8 text-base font-light leading-7 text-black last:mb-0">
+				{p.children}
+			</p>
+		);
+	},
+	ol: (ol) => {
+		return <ol className="ml-8 list-inside">{ol.children}</ol>;
+	},
+	li: (li) => {
+		return (
+			<li className="mb-6 list-decimal text-base font-light leading-7 text-black last:mb-8">
+				{li.children}
+			</li>
+		);
+	},
+	a: (a) => {
+		return (
+			<a
+				className="text-xs text-[#848280] hover:text-[#0f0700] hover:underline focus:text-[#0f0700]"
+				href={typeof a.href === 'string' ? a.href : ''}
+				target="_blank"
+			>
+				{a.children}
+			</a>
+		);
+	},
+};
+
+const getBlogPost = async (slug: string) => {
+	const blogPost = await cosmic.objects
+		.findOne({
+			type: 'blog-posts',
+			slug: slug,
+		})
+		.props(['title', 'slug', 'metadata']);
+
+	const { title, metadata } = blogPost.object;
+	const { author, date, intro, hero_image, hero_image_credit, body } = metadata;
+
+	const formattedDateString = dayjs(date).format('MMM Do, YYYY');
+
+	return {
+		title,
+		author,
+		formattedDateString,
+		intro,
+		hero_image,
+		hero_image_credit,
+		body,
+	};
+};
 
 export default async function BlogSlugPage({
 	params,
@@ -8,57 +80,79 @@ export default async function BlogSlugPage({
 	};
 }) {
 	const { slug } = params;
-	const query = `
-    {
-      blogPostCollection(where: { slug: "${slug}" }) {
-        items {
-          title
-          author
-          date
-          intro
-          postHeaderImage {
-            url
-            description
-          }
-          heroImageCredit
-          body
-        }
-      }
-    }
-  `;
-
-	const contentfulUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
-
-	const resp = await fetch(contentfulUrl, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${process.env.CONTENTFUL_DELIVERY_API_ACCESS_TOKEN}`,
-		},
-		body: JSON.stringify({ query }),
-	});
 
 	const {
-		data: {
-			blogPostCollection: { items },
-		},
-	} = await resp.json();
-
-	const { title, author, date, intro, postHeaderImage, heroImageCredit, body } =
-		items[0];
-
-	const contentHTML = marked.parse(body);
+		title,
+		author,
+		formattedDateString,
+		intro,
+		hero_image,
+		hero_image_credit,
+		body,
+	} = await getBlogPost(slug);
 
 	return (
-		<main>
-			<h1>{title}</h1>
-			<p>{author}</p>
-			<p>{date}</p>
-			<p>{intro}</p>
-			<img src={postHeaderImage.url} alt={postHeaderImage.description} />
-			<p>{heroImageCredit}</p>
+		<main className="mx-auto mb-12 mt-9 w-10/12 text-left lg:mt-24">
+			<section className="">
+				<h1 className="mb-[30px] text-xl font-semibold text-[#0f0700] lg:mx-auto lg:w-7/12 lg:text-[40px] lg:font-medium lg:leading-[55px]">
+					{title}
+				</h1>
 
-			<div dangerouslySetInnerHTML={{ __html: contentHTML }}></div>
+				{/* Author, Date, and Share Links */}
+				<div className="mb-8 flex flex-row items-center gap-4 lg:mx-auto lg:w-7/12">
+					{/* Author's image */}
+					<span
+						style={{
+							width: '46px',
+							height: '46px',
+							backgroundColor: 'yellow',
+							borderRadius: '46px',
+						}}
+						className="lg:hidden"
+					></span>
+					<span
+						style={{
+							width: '53px',
+							height: '53px',
+							backgroundColor: 'yellow',
+							borderRadius: '53px',
+						}}
+						className="hidden lg:block"
+					></span>
+
+					{/* Author name and date */}
+					<div className="flex flex-col gap-1">
+						<p className="text-base text-[#0f0700] lg:text-xl">{author}</p>
+						<p className="text-xs text-[#848280] lg:text-lg">
+							{formattedDateString}
+						</p>
+					</div>
+				</div>
+			</section>
+
+			<section className="mb-7 lg:mx-auto lg:w-7/12">
+				<ReactMarkdown components={components}>{intro}</ReactMarkdown>
+			</section>
+
+			{/* Hero Image */}
+			<section className="h-fit w-full ">
+				<div className="relative h-[600px] w-full">
+					<Image
+						className="object-cover"
+						src={hero_image.url}
+						alt={hero_image_credit}
+						fill={true}
+					/>
+				</div>
+				<ReactMarkdown components={components}>
+					{hero_image_credit}
+				</ReactMarkdown>
+			</section>
+
+			{/* Blog Post Body */}
+			<section className="mt-7 lg:mx-auto lg:w-7/12">
+				<ReactMarkdown components={components}>{body}</ReactMarkdown>
+			</section>
 		</main>
 	);
 }
