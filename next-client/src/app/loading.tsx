922736a0ec8eee35ae710c411a28be49f24c3826
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-03-23 15:28:50 -0500

    Fix top-level loading page

--- next-client/src/app/loading.tsx
+++ next-client/src/app/loading.tsx
@@ -1,8 +1,10 @@
+import { Loader2 } from 'lucide-react';
+
 export default function LoadingAboutPage() {
 	return (
-		<main className="flex min-h-screen items-center justify-center">
+		<main className="flex flex-grow flex-col items-center justify-center">
 			<h1 className="text-center text-4xl font-bold text-gray-800 dark:text-white">
-				Loading...
+				<Loader2 size={150} className="animate-spin text-green-dark" />
 			</h1>
 		</main>
 	);
