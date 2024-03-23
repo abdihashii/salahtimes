e7d6154b4176663e414154beb90b51ff0c502272
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-03-22 23:44:50 -0500

    basic linting

--- next-client/next.config.mjs
+++ next-client/next.config.mjs
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  images: {
-    remotePatterns: [{ protocol: 'https', hostname: '**' }],
-  },
+	images: {
+		remotePatterns: [{ protocol: 'https', hostname: '**' }],
+	},
 };
 
 export default nextConfig;
