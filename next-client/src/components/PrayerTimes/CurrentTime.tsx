b0e75ebe273b786f1d87984b12be55dc850fa778
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-03-23 23:27:55 -0500

    fix currentTime component for mobile

--- next-client/src/components/PrayerTimes/CurrentTime.tsx
+++ next-client/src/components/PrayerTimes/CurrentTime.tsx
@@ -3,26 +3,26 @@
 import React from 'react';
 
 const CurrentTime = ({
-  name,
-  currentTime,
+	name,
+	currentTime,
 }: {
-  name: string;
-  currentTime: string;
+	name: string;
+	currentTime: string;
 }) => {
-  return (
-    <article className="lg:min-w-3/12 mx-auto flex h-10 w-fit items-center justify-center rounded-3xl bg-green-dark lg:px-14 lg:py-3 xl:min-w-[20%]">
-      {name ? (
-        <p>
-          Current time in{' '}
-          <span>
-            {name}: {currentTime}
-          </span>
-        </p>
-      ) : (
-        <p>Please select a location</p>
-      )}
-    </article>
-  );
+	return (
+		<article className="lg:min-w-3/12 mx-auto flex h-10 w-11/12 items-center justify-center rounded-3xl bg-green-dark lg:w-fit lg:px-14 lg:py-3 xl:min-w-[20%]">
+			{name ? (
+				<p>
+					Current time in{' '}
+					<span>
+						{name}: {currentTime}
+					</span>
+				</p>
+			) : (
+				<p>Please select a location</p>
+			)}
+		</article>
+	);
 };
 
 export default CurrentTime;
