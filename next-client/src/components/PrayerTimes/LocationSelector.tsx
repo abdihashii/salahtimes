02c6a8c447f94a648acf88d6e6fd98bfef88ba80
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-03-24 00:34:52 -0500

    Make prayer times in location selector easier to see in mobile views

--- next-client/src/components/PrayerTimes/LocationSelector.tsx
+++ next-client/src/components/PrayerTimes/LocationSelector.tsx
@@ -21,17 +21,17 @@ const LocationSelector = () => {
 	return (
 		<article className="mx-auto mt-28 flex flex-col gap-10 lg:mt-52 lg:w-9/12 lg:gap-20 xl:w-7/12">
 			{/* The title of the prayer times location */}
-			<h1 className="mx-auto line-clamp-1 h-auto w-10/12 text-4xl font-medium leading-[45px] lg:text-6xl">
+			<h1 className="mx-auto line-clamp-2 h-auto w-10/12 text-4xl font-medium leading-[45px] lg:line-clamp-1 lg:text-6xl">
 				{selectedPlace.name ? (
-					<>
+					<div>
 						Prayer times in &quot;
 						<span className="text-green">{selectedPlace.name}</span>
 						&quot;
-					</>
+					</div>
 				) : (
-					<>
+					<div>
 						<span className="text-green">Prayer times</span> in your location
-					</>
+					</div>
 				)}
 			</h1>
 
@@ -53,7 +53,7 @@ const LocationSelector = () => {
 					</button>
 
 					<input
-						className="h-[2.5em] w-full rounded-full border-2 bg-white px-9 py-[11px] text-2xl text-black outline-none focus:border-green-secondary lg:h-[6.25rem] lg:w-full lg:rounded lg:px-6 lg:pb-6 lg:pt-12 lg:text-lg"
+						className="h-[2.5em] w-full overflow-ellipsis rounded-full border-2 bg-white py-[11px] pl-7 pr-16 text-2xl text-black outline-none focus:border-green-secondary lg:h-[6.25rem] lg:w-full lg:rounded lg:px-6 lg:pb-6 lg:pt-12 lg:text-lg"
 						id="autocomplete"
 						type="text"
 						value={inputValue}
@@ -65,9 +65,16 @@ const LocationSelector = () => {
 				</div>
 			</div>
 
+			{/* Current time middle of mobile view */}
+			<CurrentTime
+				className="lg:hidden"
+				name={selectedPlace.name}
+				currentTime={selectedPlace.currentTimeInLocation || ''}
+			/>
+
 			{/* Prayer times */}
 			{!prayerTimesLoading ? (
-				<div className="mx-auto grid w-full grid-cols-6 justify-between gap-6">
+				<div className="mx-auto mb-10 grid w-11/12 grid-cols-3 items-center justify-between gap-x-[15px] gap-y-[22px] lg:mb-0 lg:w-full lg:grid-cols-6 lg:gap-6">
 					{Object.entries(prayerTimes).map(([prayer, prayerTime]) => {
 						if (!prayerTime) return null;
 
@@ -81,26 +88,47 @@ const LocationSelector = () => {
 						return (
 							<div
 								key={prayer}
-								className={`flex h-10 flex-row items-center justify-center rounded-md ${prayerTimes.nextPrayer?.prayer === prayer ? 'bg-green-dark' : 'bg-white text-green-secondary'}`}
+								className={`flex h-fit flex-row items-center justify-center rounded-md lg:h-10 ${prayerTimes.nextPrayer?.prayer === prayer ? 'bg-green-dark' : 'bg-white text-black'}`}
 							>
-								{prayer}: {formatTimeString(prayerTime as string)}
+								<p className="hidden lg:block">
+									{prayer}: {formatTimeString(prayerTime as string)}
+								</p>
+
+								<div className="flex flex-col items-center rounded-[5px] py-3 lg:hidden">
+									<p className="mb-[5px] text-lg font-medium">
+										{formatTimeString(prayerTime as string)}
+									</p>
+									<p className="text-sm">{prayer}</p>
+								</div>
 							</div>
 						);
 					})}
 				</div>
 			) : (
-				<div className="mx-auto grid w-full grid-cols-6 justify-between gap-6">
-					<Skeleton className="h-10 bg-green-dark" />
-					<Skeleton className="h-10 bg-green-dark" />
-					<Skeleton className="h-10 bg-green-dark" />
-					<Skeleton className="h-10 bg-green-dark" />
-					<Skeleton className="h-10 bg-green-dark" />
-					<Skeleton className="h-10 bg-green-dark" />
-				</div>
+				<>
+					<div className="mx-auto hidden w-full grid-cols-6 justify-between gap-6 lg:grid">
+						<Skeleton className="h-10 bg-green-dark" />
+						<Skeleton className="h-10 bg-green-dark" />
+						<Skeleton className="h-10 bg-green-dark" />
+						<Skeleton className="h-10 bg-green-dark" />
+						<Skeleton className="h-10 bg-green-dark" />
+						<Skeleton className="h-10 bg-green-dark" />
+					</div>
+
+					<div className="mx-auto grid w-11/12 grid-cols-3 justify-between gap-x-[15px] gap-y-[22px] lg:hidden">
+						<Skeleton className="h-[77px] bg-green-dark" />
+						<Skeleton className="h-[77px] bg-green-dark" />
+						<Skeleton className="h-[77px] bg-green-dark" />
+						<Skeleton className="h-[77px] bg-green-dark" />
+						<Skeleton className="h-[77px] bg-green-dark" />
+						<Skeleton className="h-[77px] bg-green-dark" />
+					</div>
+				</>
 			)}
 
-			{/* Current time */}
+			{/* Current time bottom of desktop view */}
 			<CurrentTime
+				className="hidden lg:flex"
 				name={selectedPlace.name}
 				currentTime={selectedPlace.currentTimeInLocation || ''}
 			/>
