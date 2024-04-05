b1f1900b978a99b57b6d8928ff381a056cfeaa9c
Najma Yusuf <najmayusuf@Najmas-MacBook-Air.local>
2024-04-04 23:58:50 -0500

    Add docker support for the nextjs client

--- next-client/next.config.mjs
+++ next-client/next.config.mjs
@@ -2,9 +2,17 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-	images: {
-		remotePatterns: [{ protocol: 'https', hostname: '**' }],
-	},
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
+  },
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
 };
 
 export default nextConfig;
