994e12bd24c785e2e25d782a2aac8c9cf0cb8012
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-04-06 17:05:25 -0500

    fix hijri date page that expands the full screen when in mobile format

--- next-client/src/app/hijri-date/page.tsx
+++ next-client/src/app/hijri-date/page.tsx
@@ -3,19 +3,19 @@ import arabic from 'react-date-object/calendars/arabic';
 import arabic_en from 'react-date-object/locales/arabic_en';
 
 export default function HijriDatePage() {
-  const date = new DateObject({
-    format: 'MMMM D, YYYY',
-    locale: arabic_en,
-    calendar: arabic,
-  });
+	const date = new DateObject({
+		format: 'MMMM D, YYYY',
+		locale: arabic_en,
+		calendar: arabic,
+	});
 
-  return (
-    <main className="my-24 flex flex-grow flex-col items-center justify-center gap-6">
-      <h1 className="text-4xl lg:text-6xl">{date.format()}</h1>
+	return (
+		<main className="-mt-[82px] flex min-h-screen flex-grow flex-col items-center justify-center gap-6 lg:my-24 lg:min-h-0">
+			<h1 className="text-4xl lg:text-6xl">{date.format()}</h1>
 
-      <h2 className="text-lg lg:text-xl">
-        Today&apos;s Gregorian Date: {new DateObject().format('MMMM DD, YYYY')}
-      </h2>
-    </main>
-  );
+			<h2 className="text-lg lg:text-xl">
+				Today&apos;s Gregorian Date: {new DateObject().format('MMMM DD, YYYY')}
+			</h2>
+		</main>
+	);
 }
